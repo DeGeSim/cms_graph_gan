5148f93bf602a2771e4273099111b761ca74ebe5
mova <mova@users.noreply.github.com>
2021-04-21 11:14:34 +0200

    fix logger printing stuff twice

--- fgsim/utils/logger.py
+++ fgsim/utils/logger.py
@@ -3,17 +3,19 @@ import logging
 from ..config import conf
 
 logger = logging.getLogger(__name__)
-format = "%(name)s - %(levelname)s - %(message)s"
 
-logging.basicConfig(
-    filename=f"wd/{conf.tag}/log",
-    filemode="w",
-    format=format,
-)
-logger.setLevel(logging.INFO)
+if not logger.handlers:
+    format = "%(name)s - %(levelname)s - %(message)s"
+
+    logging.basicConfig(
+        filename=f"wd/{conf.tag}/log",
+        filemode="w",
+        format=format,
+    )
+    logger.setLevel(logging.INFO)
 
 
-streamhandler = logging.StreamHandler()
-formatter = logging.Formatter(format)
-streamhandler.setFormatter(formatter)
-logger.addHandler(streamhandler)
+    streamhandler = logging.StreamHandler()
+    formatter = logging.Formatter(format)
+    streamhandler.setFormatter(formatter)
+    logger.addHandler(streamhandler)
