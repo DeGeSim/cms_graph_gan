8ed53d0baf8391b9e41ff52722ccb01c830474c7
mova <mova@users.noreply.github.com>
2021-05-19 16:33:14 +0200

    effects_graph_pruning track number of nodes

--- fgsim/plot/effects_graph_pruning.py
+++ fgsim/plot/effects_graph_pruning.py
@@ -13,7 +13,16 @@ size_fractionL = []
 energyL = []
 energy_fractionL = []
 largest_graph_energy_fraction = []
-for i in range(10):
+nnodes = []
+#%%
+from matplotlib.pyplot import figure
+
+figure(figsize=(5, 5), dpi=300)
+a = np.swapaxes(excalimg[0], 0, 2)
+plt.imshow(a[15] != 0)
+plt.savefig("/home/mscham/occupation_pruning.pdf")
+#%%
+for i in range(100):
     a = np.swapaxes(excalimg[i], 0, 2)
 
     # https://stackoverflow.com/questions/62671695/convert-0-1-matrix-to-a-2d-grid-graph-in-python
@@ -23,6 +32,7 @@ for i in range(10):
     for val, node in zip(a.ravel(), sorted(G.nodes())):
         if val == 0.0:
             G.remove_node(node)
+    nnodes.append(len(G.nodes))
 
     components = list(nx.connected_components(G))  # list because it returns a generator
     complensL = [len(e) for e in components]
