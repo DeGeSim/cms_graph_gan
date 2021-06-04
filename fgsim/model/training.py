ac5846c51e7b633f2cc5f8bdf7dca23c782e822d
mova <mova@users.noreply.github.com>
2021-06-04 11:56:08 +0200

    training save the total times

--- fgsim/model/training.py
+++ fgsim/model/training.py
@@ -21,20 +21,19 @@ def diffperc(a, b):
 
 def training_procedure(c: modelHolder):
     train_loader = get_loader()
-
     # Initialize the training
     c.model = c.model.float().to(device)
     c.model.train()
     logger.info(f"Starting with epoch {c.metrics['epoch']}")
-
     skipped_to_batch = False
+
     # Iterate over the Epochs
     for c.metrics["epoch"] in range(c.metrics["epoch"], conf.model["n_epochs"]):
         # Iterate over the batches
-        endbatchtime = time.time()
+        batch_start_time = time.time()
+        saving_start_time = time.time()
         for ibatch, batch in tqdm(enumerate(train_loader)):
-            startbatchtime = time.time()
-            iotime = startbatchtime - endbatchtime
+            model_start_time = time.time()
             # skip to the correct batch
             # This construction allows restarting the training during an epoch.
             if ibatch != c.metrics["batch"] and not skipped_to_batch:
@@ -50,40 +49,32 @@ def training_procedure(c: modelHolder):
             loss.backward()
             c.optim.step()
 
-            endbatchtime = time.time()
-            grad_step_time = endbatchtime - startbatchtime
-
             c.metrics["grad_step"] = c.metrics["grad_step"] + 1
 
-            writer.add_scalar(
-                "training_time_ratio",
-                grad_step_time / (grad_step_time + iotime),
+            writer.add_scalars(
+                "times",
+                {
+                    "batch_start_time": batch_start_time,
+                    "model_start_time": model_start_time,
+                    "batchtotal": saving_start_time,
+                },
                 c.metrics["grad_step"],
             )
 
             writer.add_scalar("loss", loss, c.metrics["grad_step"])
 
-            nndiff = diffperc(prediction, batch.y.float())
-            writer.add_scalar("nndiff", nndiff, c.metrics["grad_step"])
-
-            simple_sum = global_add_pool(batch.x, batch.batch)
-            simplediff = diffperc(simple_sum, batch.y.float())
-            writer.add_scalar("simplediff", simplediff, c.metrics["grad_step"])
-
-            writer.add_scalar(
-                "nndiff - simplediff", nndiff - simplediff, c.metrics["grad_step"]
-            )
-
             writer.flush()
 
+            saving_start_time = time.time()
             # save the generated torch tensor models to disk
             if c.metrics["grad_step"] % 10 == 0:
                 logger.info(
                     f"Batch {ibatch} "
                     + f"Epoch { c.metrics['epoch'] }/{conf.model['n_epochs']}: "
-                    + f"\n\tLoss: {nndiff}"
+                    + f"\n\tLoss: {loss}"
                 )
             if c.metrics["grad_step"] % 50 == 0:
                 c.save_model()
+            batch_start_time = time.time()
         c.save_model()
         break
