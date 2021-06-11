88723f590bd3b67ff116e179624d9773af0d605c
mova <mova@users.noreply.github.com>
2021-06-11 17:11:45 +0200

    fix msgpass with static feature vector

--- fgsim/models/simple_msgpass.py
+++ fgsim/models/simple_msgpass.py
@@ -9,9 +9,10 @@ from ..geo.graph import num_node_dyn_features as initial_dyn_features
 
 nfeatures = conf.model.dyn_features + conf.model.static_features
 
-class Net(torch.nn.Module):
+
+class ModelClass(torch.nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(ModelClass, self).__init__()
         self.upscale_conv = GCNConv(initial_dyn_features, conf.model.dyn_features)
         self.conv1 = GCNConv(nfeatures, conf.model.dyn_features)
         self.conv2 = GCNConv(nfeatures, conf.model.dyn_features)
@@ -19,13 +20,18 @@ class Net(torch.nn.Module):
         self.lin = Linear(nfeatures, 1)
 
     def forward(self, batch):
-        x = self.conv1(batch.x, batch.edge_index)
+        def addstatic(x):
+            return torch.hstack((x, batch.feature_mtx_static))
+
+        x = self.upscale_conv(batch.x, batch.edge_index)
+
+        x = self.conv1(addstatic(x), batch.edge_index)
         x = F.relu(x)
 
-        x = self.conv2(x, batch.edge_index)
+        x = self.conv2(addstatic(x), batch.edge_index)
         x = F.relu(x)
 
-        x = self.conv3(x, batch.edge_index)
+        x = self.conv3(addstatic(x), batch.edge_index)
         x = F.relu(x)
 
         x = global_add_pool(x, batch.batch)
