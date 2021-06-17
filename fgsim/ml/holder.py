d44f9664e87346bea90b820118d916247bba388b
mova <mova@users.noreply.github.com>
2021-06-17 17:29:22 +0200

    add to fbf51fe - fix change by newer version  of omegaconf

--- fgsim/ml/holder.py
+++ fgsim/ml/holder.py
@@ -39,11 +39,7 @@ class modelHolder:
         # loss function
         # self.lossf = torch.nn.BCELoss().to(device)
         self.lossf = torch.nn.MSELoss().to(device)
-        self.load_checkpoint()
-
-        # state when training is started new
-        self.state = OmegaConf.merge(
-            OmegaConf.create(
+        self.state = OmegaConf.create(
                 {
                     "epoch": 0,
                     "processed_events": 0,
@@ -51,9 +47,7 @@ class modelHolder:
                     "grad_step": 0,
                     "val_losses": [],
                 }
-            ),
-            self.state,
-        )
+            )
 
     def load_checkpoint(self):
         if (
@@ -62,17 +56,9 @@ class modelHolder:
             or conf["dump_model"]
         ):
             logger.warn("Dumping model.")
-            self.state = OmegaConf.create()
             return
         self.state = OmegaConf.load(self.state_path)
         checkpoint = torch.load(self.checkpoint_path)
-
-        # self.discriminator.load_state_dict(checkpoint["discriminator"])
-        # self.discriminator.eval()
-        # self.generator.load_state_dict(checkpoint["generator"])
-        # self.generator.eval()
-        # self.optim_d.load_state_dict(checkpoint["optim_d"])
-        # self.optim_g.load_state_dict(checkpoint["optim_g"])
 
         self.model.load_state_dict(checkpoint["model"])
         self.model.eval()
@@ -127,6 +113,11 @@ class modelHolder:
             },
             self.best_model_path,
         )
+    def load_best_model(self):
+        # checkpoint = torch.load(self.best_model_path)
+        checkpoint = torch.load(self.best_model_path)
 
+        self.model.load_state_dict(checkpoint["model"])
+        self.model.eval()
 
 model_holder = modelHolder()
