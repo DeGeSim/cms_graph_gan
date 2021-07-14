edda0738ff3d185ba53093b7710e6f1bdd059062
mova <mova@users.noreply.github.com>
2021-07-14 17:01:11 +0200

    plot all available test errors

--- fgsim/plot/predict.py
+++ fgsim/plot/predict.py
@@ -1,25 +1,90 @@
-import matplotlib.pyplot as plt
-import pandas as pd
+# %%
+import os
+from glob import glob
 
-df_simple_forward = pd.read_csv(f"wd/simple_forward/prediction.csv")
-df_simple_msgpass = pd.read_csv(f"wd/simple_msgpass/prediction.csv")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from omegaconf import OmegaConf
+from scipy.stats import binned_statistic
+
+os.chdir("/home/mscham/fgsim")
+
+filepaths = glob("wd/*/*/prediction.csv")
+
+confL = [
+    OmegaConf.load(fp.rstrip("prediction.csv") + "resulting_train_config.yaml")
+    for fp in filepaths
+]
+dfL = [pd.read_csv(fp) for fp in filepaths]
+# %%
 plt.figure(num=None, figsize=(10, 5), dpi=80, facecolor="w", edgecolor="k")
-plt.scatter(
-    df_simple_forward["Energy "],
-    df_simple_forward["Prediction"],
-    label="Layerwise Pass Network",
-    alpha=0.5,
-    s=12,
-)
-plt.scatter(
-    df_simple_msgpass["Energy "],
-    df_simple_msgpass["Prediction"],
-    label="Message Pass Network",
-    alpha=0.5,
-    s=12,
-)
+for conf, df in zip(confL, dfL):
+    plt.scatter(
+        df["Energy"],
+        df["Prediction"],
+        label=conf.model.name,
+        alpha=0.1,
+        s=12,
+    )
+plt.plot([10, 510], [10, 510], label="Ideal", color="red")
 plt.xlabel("Enery")
 plt.ylabel("Prediction")
 plt.title(f"Prediction vs True Energy")
 plt.legend()
-plt.savefig(f"wd/prediction.pdf")
+plt.savefig(f"wd/prediction_vs_true_energy.pdf")
+
+# %%
+# Error plot
+
+
+bins = np.array(range(10, 520, 10))
+bincenters = (bins[1:] + bins[:-1]) / 2
+
+plt.figure(num=None, figsize=(6, 5), dpi=80, facecolor="w", edgecolor="k")
+for conf, df in zip(confL, dfL):
+    stat, edges, binnumber = binned_statistic(
+        df["Energy"], df["Relativ Error"] * 100, "mean", bins=bins
+    )
+
+    # plt.plot(
+    #     bincenters,
+    #     stat,
+    #     label=conf.model.name,
+    #     alpha=0.5,
+    # )
+    binpos, count = np.unique(binnumber - 1, return_counts=True)
+
+    xL = []
+    yL = []
+    errorL = []
+    skippedbins = 0
+    for ibin in range(len(bincenters)):
+        if binpos[ibin - skippedbins] != ibin:
+            skippedbins += 1
+            continue
+        xL.append(bincenters[ibin])
+        yL.append(stat[ibin])
+        errorL.append(1 / np.sqrt(count[ibin - skippedbins]))
+    plt.errorbar(xL, yL, yerr=np.array(errorL), label=conf.model.name)
+plt.yscale("log")
+plt.ylim(5e-1, 5e1)
+plt.xlim(0, 500)
+plt.grid(True, "both", "both")
+plt.xlabel("Energy")
+plt.ylabel("Relativ Error [%]")
+plt.title(f"Relativ Error vs True Energy")
+plt.legend()
+plt.savefig(f"wd/rel_error_vs_true_energy.pdf")
+# %%
+# pdgset = set()
+# theta = set()
+# phi = set()
+# for fp in glob("data/clic/*.h5"):
+#     with h5py.File(fp) as f:
+#         pdgset |= set(np.unique(f["pdgID"][:]))
+#         theta |= set(np.unique(f["theta"][:]))
+#         phi |= set(np.unique(f["phi"][:]))
+# print("pdgset", pdgset)
+# print("theta", theta)
+# print("phi", phi)
