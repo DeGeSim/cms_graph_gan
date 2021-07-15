bb9b5109efd4df655f535e07f140931ac15f31aa
mova <mova@users.noreply.github.com>
2021-07-15 09:43:09 +0200

    remote_run check for gpu and load correct modules

--- remote_run.sh
+++ remote_run.sh
@@ -6,8 +6,16 @@
 #SBATCH --constraint="P100"
 export LD_PRELOAD=""
 source /etc/profile.d/modules.sh
-module load maxwell gcc/9.3 cuda/11.1
-CUDA=111
+
+
+if ! command -v nvidia-smi &> /dev/null
+then
+    module load maxwell gcc/9.3
+else
+    module load maxwell gcc/9.3 cuda/11.1
+    CUDA=111
+fi
+
 TORCH=1.9.0
 cd ~/fgsim
 source .tox/py38/bin/activate
