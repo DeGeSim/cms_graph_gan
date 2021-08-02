b61cf8f3935816b26bd1e9e5e0eaf3f08e43ca1a
mova <mova@users.noreply.github.com>
2021-08-02 17:55:27 +0200

    dont monitor the validation in debug mode

--- fgsim/ml/validate.py
+++ fgsim/ml/validate.py
@@ -29,12 +29,13 @@ def validate(train_state: TrainState) -> None:
     logger.info(f"Validation Loss: {mean_loss}")
     train_state.state.val_losses.append(float(mean_loss))
 
-    train_state.writer.add_scalar(
-        "val_loss", mean_loss, train_state.state["grad_step"]
-    )
-    train_state.experiment.log_metric(
-        "val_loss", mean_loss, train_state.state["grad_step"]
-    )
+    if not conf.debug:
+        train_state.writer.add_scalar(
+            "val_loss", mean_loss, train_state.state["grad_step"]
+        )
+        train_state.experiment.log_metric(
+            "val_loss", mean_loss, train_state.state["grad_step"]
+        )
 
     mean_loss = float(mean_loss)
     if (
@@ -42,17 +43,19 @@ def validate(train_state: TrainState) -> None:
         or train_state.state.min_val_loss > mean_loss
     ):
         train_state.state.min_val_loss = mean_loss
-        train_state.experiment.log_metric("min_val_loss", mean_loss)
         train_state.state.best_grad_step = train_state.state["grad_step"]
-        train_state.experiment.log_metric(
-            "best_grad_step", train_state.state["grad_step"]
-        )
-        train_state.experiment.log_metric(
-            "best_grad_epoch", train_state.state["epoch"]
-        )
-        train_state.experiment.log_metric(
-            "best_grad_batch", train_state.state["ibatch"]
-        )
         train_state.holder.best_model_state = deepcopy(
             train_state.holder.model.state_dict()
         )
+
+        if not conf.debug:
+            train_state.experiment.log_metric("min_val_loss", mean_loss)
+            train_state.experiment.log_metric(
+                "best_grad_step", train_state.state["grad_step"]
+            )
+            train_state.experiment.log_metric(
+                "best_grad_epoch", train_state.state["epoch"]
+            )
+            train_state.experiment.log_metric(
+                "best_grad_batch", train_state.state["ibatch"]
+            )
