d5bf7b61cb5fc4fa5ce569ca36cd65765758c537
mova <mova@users.noreply.github.com>
2021-08-02 17:27:37 +0200

    fix linear regression model

--- fgsim/models/linreg.py
+++ fgsim/models/linreg.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch_geometric.nn import global_add_pool
 
 from ..config import conf
 
@@ -10,9 +9,9 @@ nfeatures = conf.model.dyn_features + conf.model.static_features
 class ModelClass(torch.nn.Module):
     def __init__(self):
         super(ModelClass, self).__init__()
-        self.end_lin = nn.Linear(1, 1)
+        self.end_lin = nn.Linear(2, 1)
 
     def forward(self, batch):
-        x = global_add_pool(batch.x, batch.batch, size=batch.num_graphs)
-        x = self.end_lin(x)
-        return x
+        X = torch.vstack([batch["ECAL_E"], batch["HCAL_E"]]).float().T
+        X = self.end_lin(X)
+        return X
