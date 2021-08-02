45bea7170258211738df00ec850b7e75132ddc7e
mova <mova@users.noreply.github.com>
2021-08-02 17:54:16 +0200

    rename config entries for early stopping

--- fgsim/ml/early_stopping.py
+++ fgsim/ml/early_stopping.py
@@ -6,23 +6,24 @@ from .train_state import TrainState
 
 
 def early_stopping(train_state: TrainState) -> bool:
-    """Compare the last conf.training.early_stopping validation losses
-    with the validation losses before that.
+    """Compare the last `conf.training.early_stopping.validation_steps`
+    validation losses with the validation losses before that.
     If the minimum has not been reduced by
-    conf.training.early_stopping_improvement, stop the training"""
-    if len(train_state.state.val_losses) < conf.training.early_stopping + 1:
+    `conf.training.early_stopping.improvement`, stop the training"""
+    valsteps = conf.training.early_stopping.validation_steps
+    if len(train_state.state.val_losses) < valsteps + 1:
         return False
     relative_improvement = 1 - (
-        min(train_state.state.val_losses[-conf.training.early_stopping :])
-        / min(train_state.state.val_losses[: -conf.training.early_stopping])
+        min(train_state.state.val_losses[-valsteps:])
+        / min(train_state.state.val_losses[:-valsteps])
     )
 
     logger.info(
         f"""\
-Relative Improvement in the last {conf.training.early_stopping} \
+Relative Improvement in the last {valsteps} \
 validation steps: {relative_improvement*100}%"""
     )
-    if relative_improvement < conf.training.early_stopping_improvement:
+    if relative_improvement < conf.training.early_stopping.improvement:
         train_state.holder.save_checkpoint()
         train_state.writer.flush()
         train_state.writer.close()
