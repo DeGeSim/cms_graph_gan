81e76f7254e6993b5c4ac86529af51ab9f73c1f6
mova <mova@users.noreply.github.com>
2021-11-30 10:13:15 +0100

    fix preprocessed seq datatype + pc loader config to proprocessed

--- fgsim/io/preprocessed_seq.py
+++ fgsim/io/preprocessed_seq.py
@@ -3,23 +3,29 @@ Here steps for reading the root files and processing the hit list \
 to graphs are definded. `process_seq` is the function that \
 should be passed the qfseq.
 """
+import importlib
 from pathlib import Path
 from typing import List
 
 import queueflow as qf
 import torch
 from torch.multiprocessing import Queue
-from torch_geometric.data import Data as GraphType
 
 from fgsim.config import conf
+
+# Import the specified processing sequence
+sel_seq = importlib.import_module(f"fgsim.io.{conf.loader.qf_seq_name}")
+
+BatchType = sel_seq.BatchType
+DataSetType = List[BatchType]
 
 # Load files
 dataset_path = Path(conf.path.training)
 dataset_path.mkdir(parents=True, exist_ok=True)
 
 # reading from the filesystem
-def read_file(file: Path) -> List[GraphType]:
-    batch_list: List[GraphType] = torch.load(file)
+def read_file(file: Path) -> DataSetType:
+    batch_list: DataSetType = torch.load(file)
     return batch_list
 
 
