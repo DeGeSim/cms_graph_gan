76c28a697950e64d6dd8bf5551f047aeed279651
mova <mova@users.noreply.github.com>
2021-12-09 11:34:50 +0100

    move to W1 val loss

---
+++ fgsim/models/loss/hlv_w1.py
@@ -0,0 +1,31 @@
+import torch
+from scipy.stats import wasserstein_distance
+
+from fgsim.io.queued_dataset import Batch
+from fgsim.ml.holder import Holder
+
+
+class LossGen:
+    def __init__(self, var: str, wgrad: bool, factor: float = 1.0, *args, **kwargs):
+        self.factor: float = factor
+        self.var: str = var
+        self.wgrad: bool = wgrad
+
+    def lossf(self, a, b):
+        a = a.cpu().numpy()
+        b = b.cpu().numpy()
+
+        return wasserstein_distance(a, b)
+
+    def __call__(self, holder: Holder, batch: Batch):
+        if self.wgrad:
+            loss = self.lossf(
+                batch.hlvs[self.var], holder.gen_points_w_grad.hlvs[self.var]
+            )
+        else:
+            with torch.no_grad():
+                loss = self.lossf(
+                    batch.hlvs[self.var], holder.gen_points.hlvs[self.var]
+                )
+
+        return self.factor * loss
