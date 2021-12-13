186b08d537a8751997f5fa394ff9da21ae3a82d5
mova <mova@users.noreply.github.com>
2021-12-13 16:02:35 +0100

    format memory manager and add documentation

--- fgsim/utils/memory.py
+++ fgsim/utils/memory.py
@@ -1,49 +1,26 @@
 import gc
-import os
 import pprint
-import sys
 from typing import Optional
 
-import psutil
 import torch
 
 from fgsim.config import device
 from fgsim.monitoring.logger import logger
 
 
-def mem_report():
-    for obj in gc.get_objects():
-        if torch.is_tensor(obj):
-            size = obj.view(-1).size()[0]
-        else:
-            size = sys.getsizeof(obj)
-        if size > 1e8:
-            if hasattr(obj, "__str__"):
-                name = obj.__str__
-            elif "__str__" in dir(obj):
-                name = obj.__str__()
-            if hasattr(obj, "name"):
-                name = obj.name
-            else:
-                name = "Unknown"
-            if torch.is_tensor(obj) or isinstance(obj, dict) or len(name) > 20:
-                logger.info(f"Type {type(obj)} {size*0.000001}MB")
-                pprint.pprint(obj)
-            else:
-                logger.info(f"{name}\t {size*0.000001}MB")
-
-
-def mem_gb():
-    # logger.debug(psutil.cpu_percent())
-    # logger.debug(psutil.virtual_memory())  # physical memory usage
-    pid = os.getpid()
-    process = psutil.Process(pid)
-    memory_use = process.memory_info()[0] / 2.0 ** 30  # memory use in GB... I think
-    # logger.debug("memory GB:", memoryUse)
-    return memory_use
-
-
 class GpuMemMonitor:
+    """
+    Context manager methods:
+    ```
+    manager = GpuMemMonitor()
+    with manager("batch"):
+        batch.to(device)
+    manager.print_recorded()
+    ```
+    `print_current_state`: print reserved/allocated/total gpu memory
+    `print_delta`: print reserved/allocated gpu memory delta
+    """
+
     def __init__(self):
         self.gpu_mem_res = torch.cuda.memory_reserved(device)
         self.gpu_mem_alloc = torch.cuda.memory_allocated(device)
@@ -57,43 +34,6 @@ class GpuMemMonitor:
         self.gpu_mem_res = torch.cuda.memory_reserved(device)
         self.gpu_mem_alloc = torch.cuda.memory_allocated(device)
 
-    def print(self, msg: Optional[str] = None):
-        self._update_mem()
-        total = torch.cuda.get_device_properties(device).total_memory
-        reserved = self.gpu_mem_res
-        allocated = self.gpu_mem_alloc
-        if msg is None:
-            msg = ""
-        else:
-            msg = msg + " "
-        logger.info(
-            f"{msg[:25]:>25} GPU Memory: reserved {reserved:.2E} allocated"
-            f" {allocated:.2E} avail {reserved-allocated:.2E} total {total:.2E}"
-        )
-
-    def print_delta(self, msg: Optional[str] = None):
-        gc.collect()
-        torch.cuda.empty_cache()
-        cur_reserved = torch.cuda.memory_reserved(device)
-        cur_alloc = torch.cuda.memory_allocated(device)
-
-        if msg is None:
-            msg = ""
-        else:
-            msg = msg + " "
-
-        logger.info(
-            f"{msg[:15]:>15} GPU Memory Δ: reserved"
-            f" {cur_reserved-self.gpu_mem_res:+.2E} allocated"
-            f" {cur_alloc-self.gpu_mem_alloc:+.2E}"
-        )
-        self.gpu_mem_res = cur_reserved
-        self.gpu_mem_alloc = cur_alloc
-
-    # Context manager methods
-    # with manager("batch"):
-    #     batch.to(device)
-    # manager.print_recorded()
     def __call__(self, varname: str, overwrite: bool = True):
         assert self.lastvar is None, "Already recording memory footprint."
         self.lastvar = varname
@@ -128,6 +68,39 @@ class GpuMemMonitor:
             f"Fraction recorded {frac_recorded*100:.0f}%, missing"
             f" {self.gpu_mem_alloc-frac_recorded:.2E}"
         )
+
+    def print_current_state(self, msg: Optional[str] = None):
+        self._update_mem()
+        total = torch.cuda.get_device_properties(device).total_memory
+        reserved = self.gpu_mem_res
+        allocated = self.gpu_mem_alloc
+        if msg is None:
+            msg = ""
+        else:
+            msg = msg + " "
+        logger.info(
+            f"{msg[:25]:>25} GPU Memory: reserved {reserved:.2E} allocated"
+            f" {allocated:.2E} avail {reserved-allocated:.2E} total {total:.2E}"
+        )
+
+    def print_delta(self, msg: Optional[str] = None):
+        gc.collect()
+        torch.cuda.empty_cache()
+        cur_reserved = torch.cuda.memory_reserved(device)
+        cur_alloc = torch.cuda.memory_allocated(device)
+
+        if msg is None:
+            msg = ""
+        else:
+            msg = msg + " "
+
+        logger.info(
+            f"{msg[:15]:>15} GPU Memory Δ: reserved"
+            f" {cur_reserved-self.gpu_mem_res:+.2E} allocated"
+            f" {cur_alloc-self.gpu_mem_alloc:+.2E}"
+        )
+        self.gpu_mem_res = cur_reserved
+        self.gpu_mem_alloc = cur_alloc
 
 
 gpu_mem_monitor = GpuMemMonitor()
