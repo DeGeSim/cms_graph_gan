d4642e4c5fa1e6691f27bf482e2e39e92b741484
mova <mova@users.noreply.github.com>
2022-01-24 18:02:00 +0100

    mean dist, fix dimension, soft by energy to improve the information the generator gets

--- fgsim/models/loss/mean_dist.py
+++ fgsim/models/loss/mean_dist.py
@@ -1,5 +1,15 @@
+"""
+We create a loss class. This class has a method `__call__`
+that takes a `Holder` and a `Batch` as arguments.
+It then computes the mean of the points in the batch
+and the mean of the points in the generated batch.
+It then uses the `torch.nn.MSELoss` to compute the loss
+between these two means. It then backpropagates the loss
+and returns the loss.
+"""
 import torch
 
+from fgsim.config import conf
 from fgsim.io.queued_dataset import Batch
 from fgsim.ml.holder import Holder
 
@@ -10,8 +20,14 @@ class LossGen:
         self.lossf = torch.nn.MSELoss()
 
     def __call__(self, holder: Holder, batch: Batch):
-        batch_means = torch.mean(batch.pc, (0, 1))
-        fake_means = torch.mean(holder.gen_points_w_grad.pc, (0, 1))
-        loss = self.factor * self.lossf(fake_means, batch_means)
+        # Aggregate the means of over the points in a event
+        sim_means = torch.mean(batch.pc, -2).sort(dim=0).values
+        gen_means = torch.mean(holder.gen_points_w_grad.pc, -2).sort(dim=0).values
+        assert (
+            list(sim_means.shape)
+            == list(gen_means.shape)
+            == [conf.loader.batch_size, conf.loader.n_features]
+        )
+        loss = self.factor * self.lossf(gen_means, sim_means)
         loss.backward(retain_graph=True)
         return float(loss)
