fix branching layer error with wrong order in generated feature vector

fgsim/ml/holder.py
@@ -126,11 +126,13 @@ class Holder:
         OmegaConf.save(config=self.state, f=conf.path.state)
 
     # Define the methods, that equip the with the generated batches
-    def gen_noise(self) -> torch.Tensor:
+    def gen_noise(self, requires_grad=False) -> torch.Tensor:
         n_features = (
             conf.loader.n_features + conf.models.gen.param.n_hidden_features
         )
-        return torch.randn(conf.loader.batch_size, 1, n_features).to(device)
+        return torch.randn(
+            conf.loader.batch_size, 1, n_features, requires_grad=requires_grad
+        ).to(device)
 
     def reset_gen_points(self) -> None:
         with torch.no_grad():
@@ -139,6 +141,6 @@ class Holder:
             self.gen_points = Batch(pc)
 
     def reset_gen_points_w_grad(self) -> None:
-        z = self.gen_noise()
+        z = self.gen_noise(True)
         pc = self.models.gen(z)
         self.gen_points_w_grad = Batch(pc)

fgsim/models/subnetworks/gen_deeptree_pc/branching.py
@@ -1,4 +1,5 @@
 from math import prod
+from typing import List
 
 import torch
 import torch.nn as nn
@@ -47,10 +48,10 @@ class BranchingLayer(nn.Module):
 
         # Add a new tree layer
         tree.append([])
-        new_features = []
-        new_edges = []
-        new_edge_attrs = []
-        new_event_idxs = []
+        new_features: List[torch.Tensor] = []
+        new_edges: List[torch.Tensor] = []
+        new_edge_attrs: List[torch.Tensor] = []
+        new_event_idxs: List[torch.Tensor] = []
         # split the nodes in the last layer
         for parent in tree[-2]:
             # Use a NN to do the splitting for each node
@@ -67,20 +68,6 @@ class BranchingLayer(nn.Module):
                 dtype=torch.long,
                 device=device,
             )
-            # # Connect the parent with the childern
-            # split_edges = torch.vstack([parent_idxs, children_idxs])
-            # new_edges.append(split_edges)
-
-            # # ### 2. Edge attrs to record the degree ###
-            # # write the degree in the edge atts
-            # new_edge_attrs.append(
-            #     torch.tensor(
-            #         1,
-            #         dtype=torch.long,
-            #         device=device,
-            #     ).repeat(self.n_branches * self.n_events)
-            # )
-
             # ### 3. Make the connections to parent in the node ###
             # Add the child to the tree to keep a reference
             for child_idxs in children_idxs.reshape(self.n_branches, -1):
@@ -118,24 +105,20 @@ class BranchingLayer(nn.Module):
             assert prod(parent_ftx_mtx.shape) * self.n_branches == prod(
                 proj_ftx.shape
             )
+            # The proj_nn projects the n_event x n_features to a
+            # n_event x (n_features*n_branches) matrix
+            # For the feature vector of the first branch, we need to slice
+            # the first n_features from this output and so on for the
+            # other branches
+            branch_ftx_list = [
+                proj_ftx[
+                    ...,
+                    self.n_features * (ibranch) : self.n_features * (1 + ibranch),
+                ]
+                for ibranch in range(self.n_branches)
+            ]
 
-            # the feature vectors for the children are in a row
-            # so we do some reshaping
-            ftx_children = proj_ftx.reshape(-1, self.n_features)
-            # assert that all the children for one event are
-            # after on another. Order for x : TreeNode>Event>Branch
-            assert torch.all(
-                proj_ftx[0]
-                == torch.hstack([ftx_children[i] for i in range(self.n_branches)])
-            )
-            assert torch.all(
-                proj_ftx[-1]
-                == torch.hstack(
-                    [ftx_children[i] for i in range(-self.n_branches, 0)]
-                )
-            )
-
-            new_features.append(ftx_children)
+            new_features.append(torch.cat(branch_ftx_list))
 
             # ### 5.b Update the event vector. ###
             # The event vector records which event a feature index belongs

tests/models/subnetworks/gen_deeptree_pc/branching.py
@@ -6,7 +6,6 @@ import torch
 from torch import nn
 from torch_geometric.data import Data
 
-from fgsim.config import device
 from fgsim.models.subnetworks.gen_deeptree_pc.branching import BranchingLayer
 from fgsim.models.subnetworks.gen_deeptree_pc.tree import Node
 
@@ -14,12 +13,19 @@ n_features = 4
 n_branches = 2
 n_global = 6
 n_events = 3
+device = torch.device("cpu")
 
 
 @pytest.fixture
 def graph():
     g = Data(
-        x=torch.randn(n_events, n_features, dtype=torch.float, device=device),
+        x=torch.randn(
+            n_events,
+            n_features,
+            dtype=torch.float,
+            device=device,
+            requires_grad=True,
+        ),
         edge_index=torch.tensor([[], []], dtype=torch.long, device=device),
         edge_attr=torch.tensor([], dtype=torch.long, device=device),
         event=torch.arange(n_events, dtype=torch.long, device=device),
@@ -28,8 +34,14 @@ def graph():
     return g
 
 
-def test_NodeSpliter(graph: Data):
-    branching_layer = BranchingLayer(
+@pytest.fixture
+def global_features():
+    return torch.randn(n_events, n_global, dtype=torch.float, device=device)
+
+
+@pytest.fixture
+def branching_layer():
+    return BranchingLayer(
         n_events=n_events,
         n_features=n_features,
         n_branches=n_branches,
@@ -38,9 +50,46 @@ def test_NodeSpliter(graph: Data):
             nn.ReLU(),
         ),
     ).to(device)
-    global_features = torch.randn(
-        n_events, n_global, dtype=torch.float, device=device
-    )
+
+
+def test_BranchingLayer_compute_graph(
+    graph: Data, branching_layer: BranchingLayer, global_features: torch.Tensor
+):
+    """
+    Make sure that the events are independent.
+    For this, we apply branching and make sure, that the gradient only
+    is nonzero for the root of the event we apply the `backwards()` on.
+    Args:
+      graph (Data): The original graph.
+      branching_layer (BranchingLayer): The branching layer to test.
+      global_features (torch.Tensor): torch.Tensor
+    """
+    new_graph1 = branching_layer(graph, global_features)
+    leaf = new_graph1.tree[-1][0]
+    pc_leaf_point = new_graph1.x[leaf.idxs[2]]
+    sum(pc_leaf_point).backward(retain_graph=True)
+
+    zero_feature = torch.zeros_like(graph.x[0])
+    assert graph.x.grad is not None
+    assert torch.all(graph.x.grad[0] == zero_feature)
+    assert torch.all(graph.x.grad[1] == zero_feature)
+    assert torch.any(graph.x.grad[2] != zero_feature)
+
+    new_graph2 = branching_layer(new_graph1, global_features)
+    leaf = new_graph2.tree[-1][0]
+    pc_leaf_point = new_graph2.x[leaf.idxs[2]]
+    sum(pc_leaf_point).backward()
+
+    assert graph.x.grad is not None
+    assert torch.all(graph.x.grad[0] == zero_feature)
+    assert torch.all(graph.x.grad[1] == zero_feature)
+    assert torch.any(graph.x.grad[2] != zero_feature)
+
+
+def test_BranchingLayer_connectivity(
+    graph: Data, branching_layer: BranchingLayer, global_features: torch.Tensor
+):
+
     for isplit in range(4):
         # ### Splitting
         graph = branching_layer(graph, global_features)

tests/models/subnetworks/gen_deeptree_pc/model.py
@@ -3,7 +3,6 @@ import torch
 from torch import nn
 from torch_geometric.data import Data
 
-from fgsim.config import device
 from fgsim.models.subnetworks.gen_deeptree_pc.ancestor_conv import AncestorConvLayer
 from fgsim.models.subnetworks.gen_deeptree_pc.branching import BranchingLayer
 from fgsim.models.subnetworks.gen_deeptree_pc.dyn_hlvs import DynHLVsLayer
@@ -13,6 +12,7 @@ n_features = 4
 n_branches = 2
 n_global = 6
 n_events = 3
+device = torch.device("cpu")
 
 
 @pytest.fixture