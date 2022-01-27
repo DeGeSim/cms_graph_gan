62f4d5c52a82db8d4ac9584f39db2a29db093f9b
mova <mova@users.noreply.github.com>
2022-01-27 11:27:39 +0100

    refactor disc treepc

--- fgsim/models/subnetworks/disc_treepc.py
+++ fgsim/models/subnetworks/disc_treepc.py
@@ -11,11 +11,12 @@ class ModelClass(nn.Module):
         self.layer_num = len(features) - 1
         super().__init__()
 
-        self.fc_layers = nn.ModuleList([])
-        for inx in range(self.layer_num):
-            self.fc_layers.append(
+        self.fc_layers = nn.ModuleList(
+            [
                 nn.Conv1d(features[inx], features[inx + 1], kernel_size=1, stride=1)
-            )
+                for inx in range(self.layer_num)
+            ]
+        )
 
         self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
         self.final_layer = nn.Sequential(
@@ -26,13 +27,13 @@ class ModelClass(nn.Module):
 
     def forward(self, f):
         feat = f.transpose(1, 2)
-        vertex_num = feat.size(2)
+        max_hits = feat.size(2)
 
         for inx in range(self.layer_num):
             feat = self.fc_layers[inx](feat)
             feat = self.leaky_relu(feat)
 
-        out = F.max_pool1d(input=feat, kernel_size=vertex_num).squeeze(-1)
+        out = F.max_pool1d(input=feat, kernel_size=max_hits).squeeze(-1)
         out = self.final_layer(out)  # (B, 1)
 
         return out
