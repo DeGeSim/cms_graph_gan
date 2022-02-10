6f11c86b97fc74a3004bd8127f93bb25f10b4b37
mova <mova@users.noreply.github.com>
2022-02-10 11:35:23 +0100

    adapt the gradient penalty to the point clouds

--- fgsim/models/loss/GradientPenalty.py
+++ fgsim/models/loss/GradientPenalty.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 import torch
 from torch.autograd import grad
 
-from fgsim.config import conf, device
-from fgsim.io.queued_dataset import Batch
+from fgsim.config import device
 from fgsim.ml.holder import Holder
+from fgsim.types import Batch, Event, Graph
 
 
 @dataclass
@@ -21,27 +21,43 @@ class LossGen:
     gamma: float = 1.0
 
     def __call__(self, holder: Holder, batch: Batch) -> torch.float:
-        real_data = batch.pc
-        alpha = torch.rand(conf.loader.batch_size, 1, 1, requires_grad=True).to(
-            device
+        gen_pc = holder.gen_points.pc
+        sim_pc = batch.pc
+        # The point could very different sizes.
+        # So we repeat the smaller one:
+        if len(gen_pc) > len(sim_pc):
+            big_pc = gen_pc
+            small_pc = sim_pc
+        else:
+            big_pc = sim_pc
+            small_pc = gen_pc
+        n_repeats = len(big_pc) // len(small_pc)
+        rest = len(big_pc) - n_repeats * len(small_pc)
+        rsmall_pc = torch.vstack(
+            [small_pc.repeat(n_repeats, 1), small_pc[:rest, :]]
         )
-        # randomly mix real and fake data
-        interpolates = real_data + alpha * (holder.gen_points.pc - real_data)
+        # Shuffle one of the datasets
+        rsmall_pc = rsmall_pc[torch.randperm(rsmall_pc.shape[0])]
+        # randomly interpolate between real and fake data
+        alpha = torch.rand(len(big_pc), 1, requires_grad=True).to(device)
+        interpolates = rsmall_pc + alpha * (big_pc - rsmall_pc)
+        interpol_batch = Batch.from_event_list(Event(Graph(x=interpolates)))
+
         # compute output of D for interpolated input
-        disc_interpolates = holder.models.disc(interpolates)
+        disc_interpolates = holder.models.disc(interpol_batch)
         # compute gradients w.r.t the interpolated outputs
 
         gradients = (
             grad(
                 outputs=disc_interpolates,
                 inputs=interpolates,
-                grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                grad_outputs=torch.ones(disc_interpolates.size(), device=device),
                 create_graph=True,
                 retain_graph=True,
                 only_inputs=True,
             )[0]
             .contiguous()
-            .view(conf.loader.batch_size, -1)
+            .view(1, -1)
         )
 
         gradient_penalty = (
