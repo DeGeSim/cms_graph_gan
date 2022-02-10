f36dc7ba5c0e33c31953d3128fc9fab08fbec0c4
mova <mova@users.noreply.github.com>
2022-02-10 11:27:22 +0100

    disc hlvs graph based

---
+++ fgsim/models/subnetworks/disc_hlvs.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+from torch_geometric.nn import global_add_pool, global_mean_pool
+
+from fgsim.config import conf
+
+
+class ModelClass(torch.nn.Module):
+    def __init__(self, activation):
+        super(ModelClass, self).__init__()
+        self.activation = getattr(torch.nn, activation)()
+        n_features = conf.loader.n_features
+        self.hlv_dnn = nn.Sequential(
+            nn.Linear(n_features * 2, n_features * 2),
+            nn.ReLU(),
+            nn.Linear(n_features * 2, n_features * 2),
+            nn.ReLU(),
+            nn.Linear(n_features * 2, n_features),
+            nn.ReLU(),
+            nn.Linear(n_features, 1),
+            nn.ReLU(),
+        )
+
+    def forward(self, batch):
+        graph = batch.graph
+        x = batch.graph.x
+        x = torch.hstack(
+            [
+                global_add_pool(x, graph.batch, size=graph.num_graphs),
+                global_mean_pool(x, graph.batch, size=graph.num_graphs),
+            ]
+        )
+        x = self.hlv_dnn(x)
+        return self.activation(x)
