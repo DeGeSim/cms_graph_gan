118403057dbe5731d6feb6cb8b030eaa780a89ca
mova <mova@users.noreply.github.com>
2022-02-14 13:38:05 +0100

    fix logglevel setting for the qf seq

--- fgsim/monitoring/logger.py
+++ fgsim/monitoring/logger.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 if not logger.handlers:
     logging.basicConfig(
         filename=conf.path.log,
+        level=logging.DEBUG if conf.loader.debug else logging.INFO,
         filemode="w",
         format="%(asctime)s - %(levelname)s - %(message)s",
         datefmt="%y-%m-%d %H:%M",
@@ -25,6 +26,6 @@ if not logger.handlers:
     qf.logger.setup_logger(
         conf.path.loader_log, print_bool=conf.loader.debug, debug=conf.loader.debug
     )
-    logger.setLevel(logging.DEBUG if conf.debug else logging.INFO)
-    logging_redirect_tqdm(logger)
+    logger.setLevel(logging.DEBUG if conf.loader.debug else logging.INFO)
+    logging_redirect_tqdm([logger])
     install_mp_handler(logger)
