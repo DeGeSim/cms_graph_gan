2fdc2d0baa4daf5e9b760778490ab8a50f8e13ac
mova <mova@users.noreply.github.com>
2022-02-14 18:13:50 +0100

    always preprocess the training batches

--- fgsim/utils/preprocess.py
+++ fgsim/utils/preprocess.py
@@ -33,21 +33,20 @@ Processing testing batches, queuing {len(data_loader.testing_chunks)} chunks."""
     torch.save(testing_batches, conf.path.test)
     logger.warning("Testing batches pickled.")
 
-    if conf.loader.preprocess_training:
-        logger.warning("Processing training batches")
-        # Turn the postprocessing off for the training
-        data_loader.postprocess_switch.value = 0
-        data_loader.queue_epoch()
-        batch_list: List[GraphType] = []
-        ifile = 0
-        for batch in tqdm(data_loader.qfseq):
-            output_file = f"{conf.path.training}/{ifile:03d}.pt"
-            batch_list.append(batch)
-            if len(batch_list) == data_loader.n_test_batches:
-                logger.info(f"Saving {output_file}")
-                torch.save(batch_list, f"{output_file}")
-                ifile += 1
-                batch_list = []
-        # Drop the last uneven set of batches
+    # if conf.loader.preprocess_training:
+    logger.warning("Processing training batches")
+    # Turn the postprocessing off for the training
+    data_loader.postprocess_switch.value = 0
+    data_loader.queue_epoch()
+    batch_list: List[GraphType] = []
+    ifile = 0
+    for batch in tqdm(data_loader.qfseq):
+        output_file = f"{conf.path.training}/{ifile:03d}.pt"
+        batch_list.append(batch)
+        if len(batch_list) == data_loader.n_test_batches:
+            logger.info(f"Saving {output_file}")
+            torch.save(batch_list, f"{output_file}")
+            ifile += 1
+            batch_list = []
     data_loader.qfseq.stop()
     exit(0)
