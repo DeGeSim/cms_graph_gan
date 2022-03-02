003505678efa3dab2e19a63b5ddbd85d444cd14d
mova <mova@users.noreply.github.com>
2022-03-02 15:15:17 +0100

    md loss over full batch with mse on mean and cov mtx values

--- fgsim/models/loss/mean_dist.py
+++ fgsim/models/loss/mean_dist.py
@@ -8,11 +8,12 @@ between these two means. It then backpropagates the loss
 and returns the loss.
 """
 import torch
-from torch_scatter import scatter_mean
 
-from fgsim.config import conf
+# from fgsim.config import conf
 from fgsim.io.sel_seq import Batch
 from fgsim.ml.holder import Holder
+
+# from torch_scatter import scatter_mean
 
 
 class LossGen:
@@ -21,20 +22,24 @@ class LossGen:
         self.lossf = torch.nn.MSELoss()
 
     def __call__(self, holder: Holder, batch: Batch):
+        gen_batch = holder.gen_points_w_grad
         # Aggregate the means of over the points in a event
-        sim_means = scatter_mean(batch.x, batch.batch, dim=0).sort(dim=0).values
-        gen_means = (
-            scatter_mean(
-                holder.gen_points_w_grad.x, holder.gen_points_w_grad.batch, dim=0
-            )
-            .sort(dim=0)
-            .values
+        # sim_means = scatter_mean(batch.x, batch.batch, dim=0).sort(dim=0).values
+        # gen_means = (
+        #     scatter_mean(gen_batch.x, gen_batch.batch, dim=0).sort(dim=0).values
+        # )
+        # assert (
+        #     list(sim_means.shape)
+        #     == list(gen_means.shape)
+        #     == [conf.loader.batch_size, conf.loader.n_features]
+        # )
+        # loss = self.factor * self.lossf(gen_means, sim_means)
+        mean_sim, mean_gen = (
+            torch.mean(sample.x, dim=0) for sample in (batch, gen_batch)
         )
-        assert (
-            list(sim_means.shape)
-            == list(gen_means.shape)
-            == [conf.loader.batch_size, conf.loader.n_features]
+        cov_sim, cov_gen = (torch.cov(sample.x.T) for sample in (batch, gen_batch))
+        loss = self.factor * (
+            self.lossf(mean_sim, mean_gen) + self.lossf(cov_sim, cov_gen)
         )
-        loss = self.factor * self.lossf(gen_means, sim_means)
         loss.backward(retain_graph=True)
         return float(loss)
