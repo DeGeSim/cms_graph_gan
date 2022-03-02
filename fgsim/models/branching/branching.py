5afb64e0afd9d587ae393aef04b12a9d336ab1a4
mova <mova@users.noreply.github.com>
2022-03-02 15:35:49 +0100

    reorganize deeptree

--- fgsim/models/branching/branching.py
+++ fgsim/models/branching/branching.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import List
 
 import torch
@@ -5,6 +6,92 @@ import torch.nn as nn
 from torch_geometric.data import Data
 
 from .tree import Node
+
+
+@dataclass
+class Tree:
+    n_events: int
+    n_features: int
+    n_branches: int
+    n_levels: int
+    device: torch.device
+
+    def __post_init__(self):
+        self.edge_index_p_level: List[torch.Tensor] = []
+        self.edge_attrs_p_level: List[torch.Tensor] = []
+        self.tree_lists: List[List[Node]] = [
+            [
+                Node(
+                    torch.arange(
+                        self.n_events, dtype=torch.long, device=self.device
+                    )
+                )
+            ]
+        ]
+        n_branches = self.n_branches
+        n_events = self.n_events
+        device = self.device
+
+        for level in range(self.n_levels):
+            # Add a new tree layer
+            self.tree_lists.append([])
+            new_edges: List[torch.Tensor] = []
+            new_edge_attrs: List[torch.Tensor] = []
+
+            # split the nodes in the last layer
+            for iparent, parent in enumerate(self.tree_lists[level]):
+                # Use a NN to do the splitting for each node
+                # output is features*splitting
+                # #### 1. Compute the new connections ###
+
+                # Calculate the index of the childern
+                last_idx_in_prev_level = (
+                    2
+                    if 2 == len(self.tree_lists)
+                    else int(self.tree_lists[level - 1][-1].idxs[-1])
+                )
+                points_in_cur_level = (
+                    n_branches * n_events * len(self.tree_lists[level - 1])
+                )
+                points_used_cur = n_branches * n_events * iparent
+                next_x_index = (
+                    last_idx_in_prev_level
+                    + points_in_cur_level
+                    + points_used_cur
+                    + 1
+                )
+
+                children_idxs = torch.arange(
+                    next_x_index,
+                    next_x_index + n_branches * n_events,
+                    dtype=torch.long,
+                    device=device,
+                )
+                # ### 3. Make the connections to parent in the node ###
+                # Add the child to the self.tree to keep a reference
+                for child_idxs in children_idxs.reshape(n_branches, -1):
+                    child = Node(child_idxs)
+                    parent.add_child(child)
+                    self.tree_lists[-1].append(child)
+
+                # ### 4. Add the connections to the ancestors ###
+                for degree, ancestor in enumerate(
+                    [parent] + parent.get_ancestors(), start=1
+                ):
+                    source_idxs = ancestor.idxs.repeat(n_branches)
+                    ancestor_edges = torch.vstack(
+                        [source_idxs, children_idxs],
+                    )
+                    new_edges.append(ancestor_edges)
+                    new_edge_attrs.append(
+                        torch.tensor(
+                            degree,
+                            dtype=torch.long,
+                            device=device,
+                        ).repeat(n_branches * n_events)
+                    )
+            self.edge_index_p_level.append(torch.hstack(new_edges))
+            self.edge_attrs_p_level.append(torch.hstack(new_edge_attrs))
 
 
 class BranchingLayer(nn.Module):
@@ -27,86 +114,12 @@ class BranchingLayer(nn.Module):
 
     def __init__(
         self,
-        *,
+        tree: Tree,
         proj_nn: nn.Module,
-        n_events: int,
-        n_features: int,
-        n_branches: int,
-        n_levels: int,
-        device: torch.device,
     ):
         super().__init__()
-        self.n_events = n_events
-        self.n_features = n_features
-        self.n_branches = n_branches
-        self.n_levels = n_levels
         self.proj_nn = proj_nn
-        self.device = device
-
-        self.edge_index_p_level: List[torch.Tensor] = []
-        self.edge_attrs_p_level: List[torch.Tensor] = []
-        self.tree: List[List[Node]] = [
-            [Node(torch.arange(n_events, dtype=torch.long, device=device))]
-        ]
-
-        for level in range(self.n_levels):
-            # Add a new tree layer
-            self.tree.append([])
-            new_edges: List[torch.Tensor] = []
-            new_edge_attrs: List[torch.Tensor] = []
-
-            # split the nodes in the last layer
-            for iparent, parent in enumerate(self.tree[level]):
-                # Use a NN to do the splitting for each node
-                # output is features*splitting
-                # #### 1. Compute the new connections ###
-
-                # Calculate the index of the childern
-                last_idx_in_prev_level = (
-                    2 if 2 == len(self.tree) else self.tree[level - 1][-1].idxs[-1]
-                )
-                points_in_cur_level = (
-                    n_branches * n_events * len(self.tree[level - 1])
-                )
-                points_used_cur = n_branches * n_events * iparent
-                next_x_index = (
-                    last_idx_in_prev_level
-                    + points_in_cur_level
-                    + points_used_cur
-                    + 1
-                )
-
-                children_idxs = torch.arange(
-                    next_x_index,
-                    next_x_index + n_branches * n_events,
-                    dtype=torch.long,
-                    device=device,
-                )
-                # ### 3. Make the connections to parent in the node ###
-                # Add the child to the self.tree to keep a reference
-                for child_idxs in children_idxs.reshape(n_branches, -1):
-                    child = Node(child_idxs)
-                    parent.add_child(child)
-                    self.tree[-1].append(child)
-
-                # ### 4. Add the connections to the ancestors ###
-                for degree, ancestor in enumerate(
-                    [parent] + parent.get_ancestors(), start=1
-                ):
-                    source_idxs = ancestor.idxs.repeat(n_branches)
-                    ancestor_edges = torch.vstack(
-                        [source_idxs, children_idxs],
-                    )
-                    new_edges.append(ancestor_edges)
-                    new_edge_attrs.append(
-                        torch.tensor(
-                            degree,
-                            dtype=torch.long,
-                            device=device,
-                        ).repeat(n_branches * n_events)
-                    )
-            self.edge_index_p_level.append(torch.hstack(new_edges))
-            self.edge_attrs_p_level.append(torch.hstack(new_edge_attrs))
+        self.tree = tree
 
     # Split each of the leafs in the the graph.tree into n_branches and connect them
     def forward(self, graph: Data, global_features: torch.Tensor) -> Data:
@@ -120,13 +133,16 @@ class BranchingLayer(nn.Module):
             isplit = graph.isplit
         del graph
 
-        n_parents = len(self.tree[isplit])
-        n_events = self.n_events
-        n_branches = self.n_branches
-        n_features = self.n_features
+        tree = self.tree.tree_lists
+        n_parents = len(tree[isplit])
+        n_events = self.tree.n_events
+        n_branches = self.tree.n_branches
+        n_features = self.tree.n_features
+        edge_index_p_level = self.tree.edge_index_p_level
+        edge_attrs_p_level = self.tree.edge_attrs_p_level
 
         # Compute the new feature vectors:
-        parents_idxs = torch.cat([parent.idxs for parent in self.tree[isplit]])
+        parents_idxs = torch.cat([parent.idxs for parent in tree[isplit]])
         # for the parents indeces generate a matrix where
         # each row is the global vector of the respective event
         parent_global = global_features[parents_idxs % n_events, :]
@@ -151,13 +167,13 @@ class BranchingLayer(nn.Module):
         )
         new_graph = Data(
             x=torch.cat([x, children_ftxs]),
-            edge_index=torch.hstack(self.edge_index_p_level[: isplit + 1]),
-            edge_attr=torch.hstack(self.edge_attrs_p_level[: isplit + 1]),
+            edge_index=torch.hstack(edge_index_p_level[: isplit + 1]),
+            edge_attr=torch.hstack(edge_attrs_p_level[: isplit + 1]),
             isplit=isplit + 1,
         )
         new_graph.event = torch.arange(
-            self.n_events, dtype=torch.long, device=device
-        ).repeat(len(new_graph.x) // self.n_events)
+            n_events, dtype=torch.long, device=device
+        ).repeat(len(new_graph.x) // n_events)
         return new_graph
 
 
--- fgsim/models/gen/gen_deeptree_pc/model.py
+++ fgsim/models/gen/gen_deeptree_pc/model.py
@@ -1,10 +1,12 @@
+from typing import Dict
+
 import torch
 import torch.nn as nn
 from torch_geometric.data import Data
 
 from fgsim.config import conf, device
 from fgsim.io.sel_seq import Batch, batch_tools
-from fgsim.models.branching.branching import BranchingLayer
+from fgsim.models.branching.branching import BranchingLayer, Tree
 from fgsim.models.branching.tree import Node
 from fgsim.models.dnn_gen import dnn_gen
 from fgsim.models.layer.ancestor_conv import AncestorConv
@@ -24,6 +26,8 @@ class ModelClass(nn.Module):
         conv_during_branching: bool,
         conv_parem,
         post_gen_mp_steps: int,
+        conv_indp: bool = False,
+        branching_indp: bool = False,
     ):
         super().__init__()
         self.n_events = n_events
@@ -34,6 +38,10 @@ class ModelClass(nn.Module):
         self.post_gen_mp_steps = post_gen_mp_steps
         self.convname = conv_name
         self.conv_during_branching = conv_during_branching
+        self.conv_indp = conv_indp
+        self.branching_indp = branching_indp
+
+        # Calculate the output points
         self.output_points = sum([n_branches ** i for i in range(self.n_levels)])
         logger.debug(f"Generator output will be {self.output_points}")
         if conf.loader.max_points > self.output_points:
@@ -49,47 +57,84 @@ class ModelClass(nn.Module):
             n_events=n_events,
         )
 
-        if self.convname == "GINConv":
-            from torch_geometric.nn.conv import GINConv
-
-            self._conv = GINConv(
-                dnn_gen(self.n_features + n_global, self.n_features, n_layers=4)
-            )
-        elif self.convname == "AncestorConv":
-            self._conv = AncestorConv(
-                n_features=self.n_features,
-                n_global=n_global,
-                **conv_parem,
-            )
-        else:
-            raise ImportError
-
-        self.branching_layer = BranchingLayer(
+        self.tree = Tree(
             n_events=self.n_events,
             n_features=self.n_features,
             n_branches=n_branches,
             n_levels=n_levels,
-            proj_nn=dnn_gen(
-                self.n_features + n_global, self.n_features * n_branches, n_layers=4
-            ),
             device=device,
         )
 
-    def conv(self, graph: Data, global_features: torch.Tensor):
-        if self.convname == "GINConv":
-            return self._conv(
-                x=torch.hstack([graph.x, global_features[graph.event]]),
-                edge_index=graph.edge_index,
+        def gen_branching_layer():
+            return BranchingLayer(
+                tree=self.tree,
+                proj_nn=dnn_gen(
+                    self.n_features + n_global,
+                    self.n_features * n_branches,
+                    n_layers=4,
+                ),
             )
 
+        if self.branching_indp:
+            self.conv_layers = [
+                gen_branching_layer() for _ in range(self.n_levels - 1)
+            ]
+        else:
+            branching_layer = gen_branching_layer()
+            self.branching_layers = [
+                branching_layer for _ in range(self.n_levels - 1)
+            ]
+
+        def gen_conv_layer():
+            if self.convname == "GINConv":
+                from torch_geometric.nn.conv import GINConv
+
+                conv = GINConv(
+                    dnn_gen(self.n_features + n_global, self.n_features, n_layers=4)
+                )
+            elif self.convname == "AncestorConv":
+                conv = AncestorConv(
+                    n_features=self.n_features,
+                    n_global=n_global,
+                    **conv_parem,
+                )
+            else:
+                raise ImportError
+            return conv
+
+        if self.conv_indp:
+            self.conv_layers = [gen_conv_layer() for _ in range(self.n_levels - 1)]
+        else:
+            conv_layer = gen_conv_layer()
+            self.conv_layers = [conv_layer for _ in range(self.n_levels - 1)]
+
+        if self.conv_indp:
+            self.pp_conv_layers = [
+                gen_conv_layer() for _ in range(self.post_gen_mp_steps)
+            ]
+        else:
+            conv_layer = gen_conv_layer()
+            self.pp_conv_layers = [
+                conv_layer for _ in range(self.post_gen_mp_steps)
+            ]
+
+    def wrap_conv(
+        self, graph: Data, global_features: torch.Tensor
+    ) -> Dict[str, torch.Tensor]:
+        if self.convname == "GINConv":
+            return {
+                "x": torch.hstack([graph.x, global_features[graph.event]]),
+                "edge_index": graph.edge_index,
+            }
+
         elif self.convname == "AncestorConv":
-            return self._conv(
-                x=graph.x,
-                edge_index=graph.edge_index,
-                edge_attr=graph.edge_attr,
-                event=graph.event,
-                global_features=global_features,
-            )
+            return {
+                "x": graph.x,
+                "edge_index": graph.edge_index,
+                "edge_attr": graph.edge_attr,
+                "event": graph.event,
+                "global_features": global_features,
+            }
 
     # Random vector to pc
     def forward(self, random_vector: torch.Tensor) -> Batch:
@@ -104,20 +149,28 @@ class ModelClass(nn.Module):
             tree=[[Node(torch.arange(n_events, dtype=torch.long, device=device))]],
         )
 
-        for _ in range(self.n_levels - 1):
+        for isplit in range(self.n_levels - 1):
             global_features = self.dyn_hlvs_layer(graph)
-            graph = self.branching_layer(graph, global_features)
+            graph = self.branching_layers[isplit](graph, global_features)
             if self.conv_during_branching:
-                graph.x = self.conv(
-                    graph,
-                    global_features,
+                graph.x = self.conv_layers[isplit](
+                    **(
+                        self.wrap_conv(
+                            graph,
+                            global_features,
+                        )
+                    )
                 )
 
-        for _ in range(self.post_gen_mp_steps):
+        for ippstep in range(self.post_gen_mp_steps):
             global_features = self.dyn_hlvs_layer(graph)
-            graph.x = self.conv(
-                graph,
-                global_features,
+            graph.x = self.pp_conv_layers[ippstep](
+                **(
+                    self.wrap_conv(
+                        graph,
+                        global_features,
+                    )
+                )
             )
         # slice the output the the corrent number of dimesions and create the batch
         batch = batch_tools.batch_from_pcs_list(
