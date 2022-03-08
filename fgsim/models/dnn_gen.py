a58eb51c3c875c69e0a62d1f672e2ce74d1530d3
mova <mova@users.noreply.github.com>
2022-03-08 11:36:59 +0100

    deeper dnns

--- fgsim/models/dnn_gen.py
+++ fgsim/models/dnn_gen.py
@@ -4,18 +4,23 @@ from torch import nn
 def dnn_gen(
     input_dim: int,
     output_dim: int,
-    n_layers: int,
+    n_layers: int = 8,
     activation_last_layer=nn.Identity(),
 ):
-    if n_layers == 1:
-        layers = [nn.Linear(input_dim, output_dim)]
-    elif n_layers == 2:
-        layers = [nn.Linear(input_dim, input_dim), nn.Linear(input_dim, output_dim)]
+    assert n_layers >= 4
+    if (input_dim + output_dim) * n_layers > 300:
+        inter_dim = max(
+            int(300 / n_layers / (input_dim + output_dim)), input_dim, output_dim
+        )
     else:
-        layers = [
-            nn.Linear(input_dim, input_dim),
-            nn.Linear(input_dim, output_dim),
-        ] + [nn.Linear(output_dim, output_dim) for _ in range(n_layers - 2)]
+        inter_dim = max(30, input_dim + output_dim)
+    print(f"inter_dim {inter_dim} nodes {inter_dim*n_layers}")
+    assert inter_dim * n_layers > 100, "Use at least 100 nodes"
+    layers = (
+        [nn.Linear(input_dim, inter_dim)]
+        + [nn.Linear(inter_dim, inter_dim) for _ in range(n_layers - 2)]
+        + [nn.Linear(inter_dim, output_dim)]
+    )
     seq = []
     for ilayer, e in enumerate(layers):
         seq.append(e)
