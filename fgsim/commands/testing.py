ab63eafa0aa114f9ceb26a001c7337535b578a58
mova <mova@users.noreply.github.com>
2022-03-25 17:27:22 +0100

    refactor testing/plotting scripts

--- fgsim/commands/testing.py
+++ fgsim/commands/testing.py
@@ -3,12 +3,13 @@ Given a trained model, it will generate a set of random events
 and compare the generated events to the simulated events.
 """
 
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List
 
 import numpy as np
 import torch
-from scipy.stats import kstest
+from scipy.stats import kstest, wasserstein_distance
 from torch_scatter import scatter_mean
 from tqdm import tqdm
 
@@ -19,15 +20,53 @@ from fgsim.ml.holder import Holder
 from fgsim.monitoring.logger import logger
 from fgsim.monitoring.train_log import TrainLog
 from fgsim.plot.hlv_marginals import hlv_marginals
-from fgsim.plot.xyscatter import xyscatter
+from fgsim.plot.xyscatter import xy_hist, xyscatter, xyscatter_faint
 
 
-def convert(tensorarr: List[torch.Tensor]) -> np.ndarray:
-    return torch.cat(tensorarr).flatten().detach().cpu().numpy()
+@dataclass
+class TestInfo:
+    train_log: TrainLog
+    sim_batches: List
+    gen_batches: List
+    hlvs_dict: Dict[str, Dict[str, np.ndarray]]
+    plot_path: Path
+    step: int
+    best_or_last: str
 
 
-def subsample(arr: np.ndarray):
-    return np.random.choice(arr, size=conf.testing.n_events, replace=False)
+def test_procedure() -> None:
+    holder: Holder = Holder()
+    train_log: TrainLog = holder.train_log
+
+    sim_batches, gen_batches_best, gen_batches_last = get_testing_datasets(holder)
+
+    for best_or_last in ["best", "last"]:
+        plot_path = Path(f"{conf.path.run_path}/plots_{best_or_last}/")
+        plot_path.mkdir(exist_ok=True)
+
+        if best_or_last == "best":
+            step = holder.state.best_grad_step
+            gen_batches = gen_batches_best
+        else:
+            step = holder.state.grad_step
+            gen_batches = gen_batches_last
+
+        hlvs_dict = compute_hlvs_dict(sim_batches, gen_batches)
+        test_info = TestInfo(
+            train_log=train_log,
+            sim_batches=sim_batches,
+            gen_batches=gen_batches,
+            hlvs_dict=hlvs_dict,
+            plot_path=plot_path,
+            step=step,
+            best_or_last=best_or_last,
+        )
+
+        test_plots(test_info)
+        if best_or_last == "best":
+            test_metrics(test_info)
+
+    exit(0)
 
 
 def get_testing_datasets(holder: Holder):
@@ -87,106 +126,159 @@ def get_testing_datasets(holder: Holder):
     return ds_dict["sim"], ds_dict["best"], ds_dict["last"]
 
 
-def test_procedure() -> None:
-    holder: Holder = Holder()
-    train_log: TrainLog = holder.train_log
+def compute_hlvs_dict(sim_batches, gen_batches) -> Dict[str, Dict[str, np.ndarray]]:
+    hlvs_dict_torch: Dict[str, Dict[str, List[torch.Tensor]]] = {
+        "gen": {},
+        "sim": {},
+    }
 
-    # if not train_log.experiment.ended:
-    #     logger.error("Training has not completed, stopping.")
-    #     loader.qfseq.stop()
-    #     exit(0)
-    sim_batches, gen_batches_best, gen_batches_last = get_testing_datasets(holder)
+    for sim_batch, gen_batch in zip(sim_batches, gen_batches):
+        for key in gen_batch.hlvs:
+            if key not in hlvs_dict_torch["sim"]:
+                hlvs_dict_torch["sim"][key] = []
+            if key not in hlvs_dict_torch["gen"]:
+                hlvs_dict_torch["gen"][key] = []
+            hlvs_dict_torch["sim"][key].append(sim_batch.hlvs[key])
+            hlvs_dict_torch["gen"][key].append(gen_batch.hlvs[key])
 
-    for best_or_last in ["best", "last"]:
-        plot_path = Path(f"{conf.path.run_path}/plots_{best_or_last}/")
-        plot_path.mkdir(exist_ok=True)
-        gen_batches = (
-            gen_batches_best if best_or_last == "best" else gen_batches_last
-        )
-
-        vals: Dict[str, Dict[str, List[torch.Tensor]]] = {"gen": {}, "sim": {}}
-
-        for sim_batch, gen_batch in zip(sim_batches, gen_batches):
-            for key in gen_batch.hlvs:
-                if key not in vals["sim"]:
-                    vals["sim"][key] = []
-                if key not in vals["gen"]:
-                    vals["gen"][key] = []
-                vals["sim"][key].append(sim_batch.hlvs[key])
-                vals["gen"][key].append(gen_batch.hlvs[key])
-
-        # # Iterate over the test sample
-        # for ibatch, batch in enumerate(tqdm(sim_batches, postfix="testing")):
-        #     with torch.no_grad():
-        #         batch = batch.clone().to(device)
-        #         holder.reset_gen_points()
-        #         holder.gen_points.compute_hlvs()
-        #         for key in holder.gen_points.hlvs:
-        #             if key not in vals["sim"]:
-        #                 vals["sim"][key] = []
-        #             if key not in vals["gen"]:
-        #                 vals["gen"][key] = []
-        #             vals["sim"][key].append(batch.hlvs[key])
-        #             vals["gen"][key].append(holder.gen_points.hlvs[key])
-
-        # Merge the computed hlvs from all batches
-
-        sample: Dict[str, Dict[str, np.ndarray]] = {
-            sim_or_gen: {
-                var: subsample(convert(vals[sim_or_gen][var]))
-                for var in vals[sim_or_gen]
-            }
-            for sim_or_gen in ("sim", "gen")
+    # Sample 2k events and plot the distribution
+    # convert to numpy
+    hlvs_dict: Dict[str, Dict[str, np.ndarray]] = {
+        sim_or_gen: {
+            var: subsample(convert(hlvs_dict_torch[sim_or_gen][var]))
+            for var in hlvs_dict_torch[sim_or_gen]
         }
-        # KS tests
-        for var in sample["gen"].keys():
-            res = kstest(sample["sim"][var], sample["gen"][var])
-            with train_log.experiment.test():
-                train_log.experiment.log_metric(f"kstest-{var}", res.pvalue)
+        for sim_or_gen in ("sim", "gen")
+    }
+    return hlvs_dict
 
-        # Scatterplots
-        logger.info(f"Plotting  xyscatter")
-        # Scatter of a single event
-        figure = xyscatter(
-            sim=sim_batches[0][0].x.numpy(),
-            gen=gen_batches[0][0].x.numpy(),
-            outputpath=plot_path / f"xyscatter_single.pdf",
-            title="Scatter event",
-        )
+
+def convert(tensorarr: List[torch.Tensor]) -> np.ndarray:
+    return torch.cat(tensorarr).flatten().detach().cpu().numpy()
+
+
+def subsample(arr: np.ndarray):
+    return np.random.choice(arr, size=conf.testing.n_events, replace=False)
+
+
+def test_metrics(test_info: TestInfo):
+    train_log = test_info.train_log
+    sim_batches = test_info.sim_batches
+    gen_batches = test_info.gen_batches
+    hlvs_dict = test_info.hlvs_dict
+    step = test_info.step
+    sim = sim_batches[0].x.numpy()
+    gen = gen_batches[0].x.numpy()
+
+    sim_means = scatter_mean(sim_batches[0].x, sim_batches[0].batch, dim=0).numpy()
+    gen_means = scatter_mean(gen_batches[0].x, gen_batches[0].batch, dim=0).numpy()
+
+    # compute a covar matrix for each batch
+    # take the sqrt of the elements to scale to the scale of the variable
+    # and then compare the distributions with w1
+
+    covars_sim = torch.vstack(
+        [torch.cov(batch.x.T).reshape(1, 4) for batch in sim_batches]
+    ).numpy()
+    covars_gen = torch.vstack(
+        [torch.cov(batch.x.T).reshape(1, 4) for batch in gen_batches]
+    ).numpy()
+
+    metrics_dict = {
+        "w1_x": wasserstein_distance(sim[:, 0], gen[:, 0]),
+        "w1_y": wasserstein_distance(sim[:, 1], gen[:, 1]),
+        "w1_x_means": wasserstein_distance(sim_means[:, 0], gen_means[:, 0]),
+        "w1_y_means": wasserstein_distance(sim_means[:, 1], gen_means[:, 1]),
+        "w1_cov_xx": wasserstein_distance(covars_sim[:, 0], covars_gen[:, 0]),
+        "w1_cov_xy": wasserstein_distance(covars_sim[:, 1], covars_gen[:, 1]),
+        "w1_cov_yx": wasserstein_distance(covars_sim[:, 2], covars_gen[:, 2]),
+        "w1_cov_yy": wasserstein_distance(covars_sim[:, 3], covars_gen[:, 3]),
+    }
+    # KS tests
+    for var in hlvs_dict["gen"].keys():
+        res = kstest(hlvs_dict["sim"][var], hlvs_dict["gen"][var])
         with train_log.experiment.test():
-            train_log.experiment.log_figure(
-                figure_name=f"test-xyscatter", figure=figure, overwrite=True
-            )
-
-        figure = xyscatter(
-            sim=sim_batches[0].x.numpy(),
-            gen=gen_batches[0].x.numpy(),
-            outputpath=plot_path / f"xyscatter_batch.pdf",
-            title="Scatter all points in batch",
+            train_log.experiment.log_metric(f"kstest-{var}", res.pvalue)
+    with train_log.experiment.test():
+        train_log.experiment.log_metrics(
+            metrics_dict,
+            step=step,
         )
 
-        figure = xyscatter(
-            sim=scatter_mean(sim_batches[0].x, sim_batches[0].batch, dim=0).numpy(),
-            gen=scatter_mean(gen_batches[0].x, gen_batches[0].batch, dim=0).numpy(),
-            outputpath=plot_path / f"xyscatter_batch_means.pdf",
-            title=f"Event means for a batch ({len(sim_batches[0].batch)})",
-        )
-        with train_log.experiment.test():
-            train_log.experiment.log_figure(
-                figure_name=f"xyscatter_batch_means", figure=figure, overwrite=True
-            )
 
-        # # Sample 2k events and plot the distribution
-        for var in sample["gen"]:
-            xsim = sample["sim"][var]
-            xgen = sample["gen"][var]
-            logger.info(f"Plotting  var {var}")
-            figure = hlv_marginals(
-                var, xsim=xsim, xgen=xgen, outputpath=plot_path / f"{var}.pdf"
-            )
+def test_plots(test_info: TestInfo):
+    train_log = test_info.train_log
+    sim_batches = test_info.sim_batches
+    gen_batches = test_info.gen_batches
+    hlvs_dict = test_info.hlvs_dict
+    plot_path = test_info.plot_path
+    best_or_last = test_info.best_or_last
+
+    from torch_geometric.data import Batch
+
+    sim_batches_stacked = Batch.from_data_list(
+        [e for ee in sim_batches for e in ee.to_data_list()]
+    )
+    gen_batches_stacked = Batch.from_data_list(
+        [e for ee in gen_batches for e in ee.to_data_list()]
+    )
+
+    def log_figure(figure, filename):
+        if best_or_last == "best":
             with train_log.experiment.test():
                 train_log.experiment.log_figure(
-                    figure_name=f"distplot-{var}", figure=figure, overwrite=True
+                    figure_name=filename, figure=figure, overwrite=True
                 )
+        logger.info(plot_path / filename)
 
-    exit(0)
+    # Scatter of a single event
+    figure = xyscatter(
+        sim=sim_batches[0][0].x.numpy(),
+        gen=gen_batches[0][0].x.numpy(),
+        outputpath=plot_path / f"xyscatter_single.pdf",
+        title=f"Scatter a single event ({conf.loader.max_points} points)",
+    )
+    log_figure(figure, "xyscatter_single.pdf")
+
+    figure = xyscatter_faint(
+        sim=sim_batches[0].x.numpy(),
+        gen=gen_batches[0].x.numpy(),
+        outputpath=plot_path / f"xyscatter_batch.pdf",
+        title=(
+            f"Scatter points ({conf.loader.max_points}) in batch"
+            f" ({conf.loader.batch_size})"
+        ),
+    )
+    log_figure(figure, "xyscatter_batch.pdf")
+
+    figure = xy_hist(
+        sim=sim_batches_stacked.x.numpy(),
+        gen=gen_batches_stacked.x.numpy(),
+        outputpath=plot_path / f"xy_hist.pdf",
+        title=(
+            f"2D Histogram for {conf.loader.max_points} points in"
+            f" {conf.testing.n_events} events"
+        ),
+    )
+    log_figure(figure, "xy_hist.pdf")
+
+    figure = xyscatter(
+        sim=scatter_mean(
+            sim_batches_stacked.x, sim_batches_stacked.batch, dim=0
+        ).numpy(),
+        gen=scatter_mean(
+            gen_batches_stacked.x, gen_batches_stacked.batch, dim=0
+        ).numpy(),
+        outputpath=plot_path / f"xy_event_means.pdf",
+        title=f"Event means for ({conf.testing.n_events}) events",
+    )
+    log_figure(figure, "xy_event_means.pdf")
+
+    for var in hlvs_dict["gen"]:
+        xsim = hlvs_dict["sim"][var]
+        xgen = hlvs_dict["gen"][var]
+        logger.info(f"Plotting  var {var}")
+        figure = hlv_marginals(
+            var, xsim=xsim, xgen=xgen, outputpath=plot_path / f"{var}.pdf"
+        )
+        log_figure(figure, f"{var}.pdf")
--- fgsim/plot/xyscatter.py
+++ fgsim/plot/xyscatter.py
@@ -43,9 +43,92 @@ def xyscatter(
         borderaxespad=0,
         labels=[f"gen μ{mean_gen}\nσ{cov_gen}", f"sim μ{mean_sim}\nσ{cov_sim}"],
     )
-    plt.tight_layout()
 
     g.savefig(outputpath)
     g.savefig(outputpath.with_suffix(".png"))
 
     return g.figure
+
+
+def xyscatter_faint(
+    sim: np.array, gen: np.array, outputpath: Path, title: str
+) -> plt.Figure:
+    np.set_printoptions(formatter={"float_kind": "{:.3g}".format})
+    mean_sim = np.around(np.mean(sim, axis=0), 2)
+    cov_sim = str(np.around(np.cov(sim, rowvar=0), 2)).replace("\n", "")
+    mean_gen = np.around(np.mean(gen, axis=0), 2)
+    cov_gen = str(np.around(np.cov(gen, rowvar=0), 2)).replace("\n", "")
+    sim_df = pd.DataFrame(
+        {
+            "x": sim[:, 0],
+            "y": sim[:, 1],
+            "cls": f"sim μ{mean_sim}\nσ{cov_sim}",
+        }
+    )
+    gen_df = pd.DataFrame(
+        {
+            "x": gen[:, 0],
+            "y": gen[:, 1],
+            "cls": f"gen μ{mean_gen}\nσ{cov_gen}",
+        }
+    )
+    df = pd.concat([sim_df, gen_df], ignore_index=True)
+
+    plt.cla()
+    plt.clf()
+    g: sns.JointGrid = sns.jointplot(
+        data=df, x="x", y="y", hue="cls", alpha=0.1, legend=False
+    )
+    g.fig.suptitle(title)
+    # g.ax_joint.collections[0].set_alpha(0)
+    # g.fig.tight_layout()
+    g.figure.subplots_adjust(top=0.95)
+    plt.legend(
+        bbox_to_anchor=(1.01, 1),
+        borderaxespad=0,
+        labels=[f"gen μ{mean_gen}\nσ{cov_gen}", f"sim μ{mean_sim}\nσ{cov_sim}"],
+    )
+
+    g.savefig(outputpath)
+    g.savefig(outputpath.with_suffix(".png"))
+
+    return g.figure
+
+
+def xy_hist(
+    sim: np.array, gen: np.array, outputpath: Path, title: str
+) -> plt.Figure:
+    np.set_printoptions(formatter={"float_kind": "{:.3g}".format})
+    mean_sim = np.around(np.mean(sim, axis=0), 2)
+    cov_sim = str(np.around(np.cov(sim, rowvar=0), 2)).replace("\n", "")
+    mean_gen = np.around(np.mean(gen, axis=0), 2)
+    cov_gen = str(np.around(np.cov(gen, rowvar=0), 2)).replace("\n", "")
+    sim_df = pd.DataFrame(
+        {
+            "x": sim[:, 0],
+            "y": sim[:, 1],
+            "cls": f"sim μ{mean_sim}\nσ{cov_sim}",
+        }
+    )
+    gen_df = pd.DataFrame(
+        {
+            "x": gen[:, 0],
+            "y": gen[:, 1],
+            "cls": f"gen μ{mean_gen}\nσ{cov_gen}",
+        }
+    )
+    plt.cla()
+    plt.clf()
+
+    sns.set()
+    fig, axes = plt.subplots(1, 2)
+    axes[0].hist2d(sim_df["x"], sim_df["y"], bins=[100, 100])
+    axes[0].set_title("sim")
+    axes[1].hist2d(gen_df["x"], gen_df["y"], bins=[100, 100])
+    axes[1].set_title("gen")
+    fig.suptitle(title)
+
+    fig.savefig(outputpath)
+    fig.savefig(outputpath.with_suffix(".png"))
+
+    return fig
