8b9855dc60a12b4df2480f1adcd81ce84ae13386
mova <mova@users.noreply.github.com>
2022-05-17 16:36:17 +0200

    mmd configureable

--- fgsim/models/loss/mmd.py
+++ fgsim/models/loss/mmd.py
@@ -1,4 +1,7 @@
+from typing import List
+
 import torch
+from everett import ConfigurationError
 
 from fgsim.config import conf, device
 from fgsim.io.sel_seq import Batch
@@ -6,9 +9,10 @@ from fgsim.ml.holder import Holder
 
 
 class LossGen:
-    def __init__(self, factor: float, kernel: str = "rbf") -> None:
+    def __init__(self, factor: float, kernel, bandwidth: List[float]) -> None:
         self.factor = factor
         self.kernel = kernel
+        self.bandwidth = bandwidth
 
     def __call__(self, holder: Holder, batch: Batch, *args, **kwargs):
         shape = (
@@ -19,53 +23,47 @@ class LossGen:
         gen_sample = holder.gen_points_w_grad.x.reshape(*shape)
         assert sim_sample.shape == gen_sample.shape
 
-        loss = MMD(
+        loss = self.MMD(
             sim_sample,
             gen_sample,
-            kernel=self.kernel,
         )
         loss.backward(retain_graph=True)
         return float(loss)
 
+    def MMD(self, x, y):
+        # https://www.kaggle.com/code/onurtunali/maximum-mean-discrepancy/notebook
+        """Emprical maximum mean discrepancy. The lower the result
+        the more evidence that distributions are the same.
 
-def MMD(x, y, kernel):
-    # https://www.kaggle.com/code/onurtunali/maximum-mean-discrepancy/notebook
-    """Emprical maximum mean discrepancy. The lower the result
-       the more evidence that distributions are the same.
+        Args:
+            x: first sample, distribution P
+            y: second sample, distribution Q
+            kernel: kernel type such as "multiscale" or "rbf"
+        """
+        xx, yy, zz = torch.mm(x, x.t()), torch.mm(y, y.t()), torch.mm(x, y.t())
+        rx = xx.diag().unsqueeze(0).expand_as(xx)
+        ry = yy.diag().unsqueeze(0).expand_as(yy)
 
-    Args:
-        x: first sample, distribution P
-        y: second sample, distribution Q
-        kernel: kernel type such as "multiscale" or "rbf"
-    """
-    xx, yy, zz = torch.mm(x, x.t()), torch.mm(y, y.t()), torch.mm(x, y.t())
-    rx = xx.diag().unsqueeze(0).expand_as(xx)
-    ry = yy.diag().unsqueeze(0).expand_as(yy)
+        dxx = rx.t() + rx - 2.0 * xx  # Used for A in (1)
+        dyy = ry.t() + ry - 2.0 * yy  # Used for B in (1)
+        dxy = rx.t() + ry - 2.0 * zz  # Used for C in (1)
 
-    dxx = rx.t() + rx - 2.0 * xx  # Used for A in (1)
-    dyy = ry.t() + ry - 2.0 * yy  # Used for B in (1)
-    dxy = rx.t() + ry - 2.0 * zz  # Used for C in (1)
+        XX, YY, XY = (
+            torch.zeros(xx.shape).to(device),
+            torch.zeros(xx.shape).to(device),
+            torch.zeros(xx.shape).to(device),
+        )
 
-    XX, YY, XY = (
-        torch.zeros(xx.shape).to(device),
-        torch.zeros(xx.shape).to(device),
-        torch.zeros(xx.shape).to(device),
-    )
+        for a in self.bandwidth:
+            if self.kernel == "multiscale":
+                XX += a ** 2 * (a ** 2 + dxx) ** -1
+                YY += a ** 2 * (a ** 2 + dyy) ** -1
+                XY += a ** 2 * (a ** 2 + dxy) ** -1
+            elif self.kernel == "rbf":
+                XX += torch.exp(-0.5 * dxx / a)
+                YY += torch.exp(-0.5 * dyy / a)
+                XY += torch.exp(-0.5 * dxy / a)
+            else:
+                raise ConfigurationError
 
-    if kernel == "multiscale":
-
-        bandwidth_range = [0.2, 0.5, 0.9, 1.3]
-        for a in bandwidth_range:
-            XX += a ** 2 * (a ** 2 + dxx) ** -1
-            YY += a ** 2 * (a ** 2 + dyy) ** -1
-            XY += a ** 2 * (a ** 2 + dxy) ** -1
-
-    if kernel == "rbf":
-
-        bandwidth_range = [10, 15, 20, 50]
-        for a in bandwidth_range:
-            XX += torch.exp(-0.5 * dxx / a)
-            YY += torch.exp(-0.5 * dyy / a)
-            XY += torch.exp(-0.5 * dxy / a)
-
-    return torch.mean(XX + YY - 2.0 * XY)
+        return torch.mean(XX + YY - 2.0 * XY)
