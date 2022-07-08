eeacb1403431aa85a19b17b6ac7f8a215735052d
mova <mova@users.noreply.github.com>
2022-07-08 15:59:50 +0200

    add cd loss

---
+++ fgsim/models/loss/cd.py
@@ -0,0 +1,20 @@
+from fgsim.io.sel_seq import Batch
+from fgsim.ml.holder import Holder
+from fgsim.models.metrics.dcd import cd
+
+
+class LossGen:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, holder: Holder, batch: Batch):
+        # Loss of the generated samples
+
+        n_features = batch.x.shape[1]
+        batch_size = int(batch.batch[-1] + 1)
+        loss = cd(
+            holder.gen_points_w_grad.x.reshape(batch_size, -1, n_features),
+            batch.x.reshape(batch_size, -1, n_features),
+        )
+
+        return loss
