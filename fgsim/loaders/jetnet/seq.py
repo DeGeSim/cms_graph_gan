fix jetnet dataloader with masking

fgsim/loaders/jetnet/seq.py
@@ -10,6 +10,8 @@ from torch_geometric.data import Batch, Data
 from fgsim.config import conf
 from fgsim.io.batch_tools import compute_hlvs
 
+from .scaler import scaler
+
 # Sharded switch for the postprocessing
 postprocess_switch = Value("i", 0)
 
@@ -48,18 +50,28 @@ def read_chunk(chunks: ChunkType):
                 file_path,
                 skiprows=start,
                 nrows=(end - start),
-                sep=",",
-                # header=0,
+                sep=" ",
+                header=None,
                 # index_col=0,
             )
         )
-    res = pd.concat(chunks_list).values.reshape(-1, 30, 3)
+    res = pd.concat(chunks_list).values.reshape(-1, 30, 4)
     # res = res[..., :3]
     return torch.tensor(res).float()
 
 
-def transform(pc):
-    graph = Data(x=pc)
+def transform(pc) -> Data:
+    graph = contruct_graph_from_row(pc)
+    if postprocess_switch.value:
+        graph.hlvs = compute_hlvs(graph)
+    graph.x[graph.mask] = torch.from_numpy(
+        scaler.transform(graph.x[graph.mask].numpy())
+    ).float()
+    return graph
+
+
+def transform_wo_scaling(pc) -> Data:
+    graph = contruct_graph_from_row(pc)
     if postprocess_switch.value:
         graph.hlvs = compute_hlvs(graph)
     return graph
@@ -72,3 +84,9 @@ def aggregate_to_batch(list_of_events) -> Batch:
 
 def magic_do_nothing(batch: Batch) -> Batch:
     return batch
+
+
+def contruct_graph_from_row(row) -> Data:
+    res = Data(x=row[:, :3], mask=row[:, :1].reshape(-1).bool())
+    res.x[~res.mask] = -5.0
+    return res