32dfdc003472bb6d6d4aa57272a33a8bc7f32a77
mova <mova@users.noreply.github.com>
2022-07-29 11:46:28 +0200

    conda setup directly in ram - cometml still not working

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
-set -e
-module purge
-conda init zsh
-conda install mamba
-mamba init
-set -x
-mamba install -c conda-forge conda-pack
-mamba create --name fgsim python=3.9
+set -ex
 
-mamba activate fgsim
-mamba install pytorch=1.11 cudatoolkit=11.3 -c pytorch
-mamba install numpy
+export CONDA_DIR=~/beegfs/conda/miniconda
+export RAMDIR=/dev/shm/$USER/fgsim
+export TARBALL_PATH=~/beegfs/conda/fgsim.tar
+export TARBALL_BASE_PATH=~/beegfs/conda/fgsim_base.tar
+mkdir -p /dev/shm/$USER
+
+# module purge
+# # conda init zsh
+# conda install mamba --yes
+# # mamba init
+# # mamba install -c conda-forge conda-pack
+#
+# mamba create --yes --prefix /dev/shm/$USER/fgsim python=3.9
+
+# Init conda and mamba by hand
+set +x
+eval "$(${CONDA_DIR}/bin/conda shell.bash hook 2> /dev/null)"
+source ${CONDA_DIR}/etc/profile.d/mamba.sh
+set -x
+
+mamba activate /dev/shm/mscham/fgsim
+mamba install --yes  -c pytorch  pytorch=1.11 cudatoolkit=11.3
+mamba install --yes numpy
 python -c 'import torch; assert torch.cuda.is_available()'
-mamba install glib
-mamba install pyg -c pyg
+# mamba install glib
+mamba install --yes pyg -c pyg
 # Assert that torch scatter works
 python -c 'import torch_scatter'
 
 
 #Project dependencies
-mamba install omegaconf typeguard tqdm uproot awkward tensorboard tblib pytorch-lightning scikit-learn multiprocessing-logging  icecream prettytable pretty_errors
-mamba install -c comet_ml comet_ml
+mamba install --yes omegaconf typeguard tqdm uproot awkward tensorboard tblib pytorch-lightning scikit-learn multiprocessing-logging  icecream prettytable pretty_errors
+mamba install --yes -c comet_ml comet_ml
 # dev tools
-mamba install black isort flake8 mypy pytest pre-commit ipykernel
+mamba install --yes black isort flake8 mypy pytest pre-commit ipykernel
 
 # jetnet requirements
-mamba install coffea h5py wurlitzer
+mamba install --yes coffea h5py wurlitzer
 pip install jetnet torchtyping
 
-export TARBALL_BASE_PATH=~/beegfs/conda/fgsim_base.tar
-export TARBALL_PATH=~/beegfs/conda/fgsim.tar
-export RAMDIR=/dev/shm/$USER/fgsim
 
-mamba pack -n fgsim -o ${TARBALL_BASE_PATH}
+# mamba pack -n fgsim -o ${TARBALL_BASE_PATH}
 
-
-mkdir -p ${RAMDIR}
 tar -x -f ${TARBALL_BASE_PATH} --directory ${RAMDIR}
 
 # activate the enviroment in ram
