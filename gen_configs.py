594d06ec60f95cba0970a8fb8815a6e5706819ce
mova <mova@users.noreply.github.com>
2022-08-03 12:15:39 +0200

    genconfig for jetnet

--- gen_configs.py
+++ gen_configs.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from os import mkdir
 from typing import Callable, Dict, List
@@ -20,17 +21,26 @@ class ExperimentOption:
 base_config = ExperimentConfig(
     OmegaConf.create(
         {
-            "comet_project_name": "moons-without-disc",
             "models": {
-                "gen": {"losses_list": ["mmdpc", "outside_interval"]},
+                "gen": {
+                    "optim": {
+                        "params": {
+                            "lr": 2.0e-4,
+                        }
+                    },
+                },
                 "disc": {
                     "name": "disc_fake",
-                    "losses_list": [],
+                    "optim": {
+                        "params": {
+                            "lr": 2.0e-4,
+                        }
+                    },
                 },
             },
         }
     ),
-    [],
+    ["jetnet"],
 )
 
 exp_list: List[ExperimentConfig] = [base_config]
@@ -42,50 +52,43 @@ def add_option(option: Callable):
     optionslist.append(option)
 
 
-# %%
-# @add_option
-# def option_disc(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
-#     input_conf = exp_config.config
-#     return {"disctpc": input_conf}
+@add_option
+def option_disc(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
+    res = defaultdict(exp_config.config.copy)
+    res["dmp"]["models"]["disc"]["name"] = "disc_mp"
+    res["dpnet"]["models"]["disc"]["name"] = "disc_pointnetmix"
+    res["dpcgan"]["models"]["disc"]["name"] = "disc_pcgan"
+    return res
 
 
 @add_option
-def option_gen(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
-    input_conf = exp_config.config
-    mod_conf = input_conf.copy()
-    input_conf["models"]["gen"]["name"] = "gen_deeptree"
-    mod_conf["models"]["gen"]["name"] = "gen_linear"
-    return {"genlin": mod_conf}  # "gentd": input_conf,
+def option_disc_lr(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
+    res = defaultdict(exp_config.config.copy)
+    # res["lrddef"]["models"]["disc"]["optim"]["params"]["lr"] = 2.0e-4
+    res["lrdlow"]["models"]["disc"]["optim"]["params"]["lr"] = 2.0e-5
+    res["lrdvlow"]["models"]["disc"]["optim"]["params"]["lr"] = 2.0e-6
+    return res
 
 
 @add_option
-def option_kernel(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
-    input_conf = exp_config.config
-    input_conf["loss_options"] = {"mmdpc": {"kernel": "rbf"}}
-    mod_conf = input_conf.copy()
-    mod_conf["loss_options"] = {"mmdpc": {"kernel": "multiscale"}}
-    return {
-        "rbf": input_conf,
-    }  # "multiscale": mod_conf
+def option_gen_lr(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
+    res = defaultdict(exp_config.config.copy)
+    res["lrgdef"]["models"]["gen"]["optim"]["params"]["lr"] = 2.0e-4
+    res["lrglow"]["models"]["gen"]["optim"]["params"]["lr"] = 6.0e-5
+    # res["lrgvlow"]["models"]["gen"]["optim"]["params"]["lr"] = 1.0e-5
+    return res
 
 
 @add_option
-def option_bandwidth(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
-    outdict = {}
-    if exp_config.config["loss_options"]["mmdpc"]["kernel"] == "rbf":
-        for bw in [10, 15, 20, 50]:  # [30, 40, 70, 90]:
-            mod_conf = exp_config.config.copy()
-            mod_conf["loss_options"] = {"mmdpc": {"bandwidth": [bw]}}
-            outdict[f"bw-{bw}"] = mod_conf
-    else:
-        for bw in [0.01, 0.05, 0.1, 0.5, 0.8, 1.5, 3]:
-            mod_conf = exp_config.config.copy()
-            mod_conf["loss_options"] = {"mmdpc": {"bandwidth": [bw]}}
-            outdict[f"bw-{bw}"] = mod_conf
-    return outdict
+def option_optimizer(exp_config: ExperimentConfig) -> Dict[str, DictConfig]:
+    res = defaultdict(exp_config.config.copy)
+    # res["Adamnodecay"]
+    res["Adam"]["models"]["gen"]["optim"]["params"]["weight_decay"] = 1.0e-4
+    res["Adam"]["models"]["disc"]["optim"]["params"]["weight_decay"] = 1.0e-4
+    res["RMSprop"]["models"]["gen"]["optim"]["name"] = "RMSprop"
+    res["RMSprop"]["models"]["disc"]["optim"]["name"] = "RMSprop"
+    return res
 
-
-# %%
 
 for option in optionslist:
     new_exp_list = []
@@ -100,13 +103,6 @@ for option in optionslist:
                 )
     exp_list = new_exp_list
 
-
-# for exp in exp_list:
-#     exp.config = compute_update_config(
-#         base_config.config,
-#         exp.config,
-#     )
-
 for exp in exp_list:
     print("_".join(exp.tags))
     print(exp.config)
@@ -119,5 +115,3 @@ print(
     + ",".join(["_".join(exp.tags) for exp in exp_list])
     + " train"
 )
-
-# %%
