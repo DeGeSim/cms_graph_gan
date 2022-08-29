9c0e74cf65197996e8c7d06fde73f32e4fffd4f2
mova <mova@users.noreply.github.com>
2022-08-29 14:36:44 +0200

    jetnet util batch data type

--- fgsim/utils/jetnetutils.py
+++ fgsim/utils/jetnetutils.py
@@ -1,9 +1,11 @@
+from typing import Union
+
 import torch
-from torch_geometric.data import Batch
+from torch_geometric.data import Batch, Data
 from torch_geometric.utils import to_dense_batch
 
 
-def _shape_from_batch(batch: Batch):
+def _shape_from_batch(batch: Union[Data, Batch]):
     # batch_size = int(batch.batch[-1] + 1)
     # n_features = int(batch.x.shape[-1])
     # assert batch.x.shape[0] % batch_size == 0
@@ -11,12 +13,12 @@ def _shape_from_batch(batch: Batch):
     return (-1, 30, 3)
 
 
-def _to_stacked_mask(batch: Batch):
+def _to_stacked_mask(batch: Union[Data, Batch]):
     x, mask = to_dense_batch(x=batch.x, batch=batch.batch, max_num_nodes=30)
     return torch.cat((x, mask.float().reshape(-1, 30, 1)), dim=2)
 
 
-def _pp_for_jetnet_metric(batch: Batch) -> torch.Tensor:
+def _pp_for_jetnet_metric(batch: Union[Data, Batch]) -> torch.Tensor:
     shape = list(_shape_from_batch(batch))
     x = _to_stacked_mask(batch)
     shape[-1] += 1  # increse for the mask
