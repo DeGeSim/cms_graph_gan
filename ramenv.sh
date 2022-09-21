e69c4e98d6eddb8f7677e35eeeb562e39c683024
mova <mova@users.noreply.github.com>
2022-09-21 13:51:01 +0200

    lock for ramenv

--- ramenv.sh
+++ ramenv.sh
@@ -3,11 +3,19 @@ ulimit -n `ulimit -H -n`
 
 RAMPATH="/dev/shm/$USER/fgsim"
 
+# # Get the file descript and lock the file
+# LOCKPATH=/dev/shm/${USER}/ramenv.lock
+# exec 4> ${LOCKPATH} || exit 1
+# flock 4 || exit 1
+# trap "rm -f ${LOCKPATH}" EXIT
+
 if [ ! -d $RAMPATH ]; then
   echo "Installing  $RAMPATH"
   mkdir -p $RAMPATH
   tar -x -f ~/fgsim/env.tar --directory ${RAMPATH}
 fi
+
+# remove aliases that break the conda setup script
 if alias ls &>/dev/null ; then
   unalias ls
 fi
@@ -17,6 +25,5 @@ export CONDA_DIR=~/beegfs/conda/miniconda
 eval "$(${CONDA_DIR}/bin/conda shell.bash hook 2> /dev/null)"
 source ${CONDA_DIR}/etc/profile.d/mamba.sh
 mamba activate fgsim
-
 # save ram env
 # tar -c -f ~/fgsim/env.tar --directory=${RAMPATH} .
