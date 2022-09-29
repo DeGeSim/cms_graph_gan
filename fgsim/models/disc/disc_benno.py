84bfe2cbf62457e9491ef7f05614c1c0825a130f
mova <mova@users.noreply.github.com>
2022-09-29 17:39:46 +0200

    add benno discriminator

---
+++ fgsim/models/disc/disc_benno.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+from torch.nn.functional import leaky_relu
+
+from fgsim.utils.jetnetutils import to_stacked_mask
+
+
+class ModelClass(nn.Module):
+    def __init__(self):
+        super(ModelClass, self).__init__()
+        self.net = Disc()
+
+    def forward(self, batch, cond):
+        x = to_stacked_mask(batch)
+        x = self.net(x[..., :3])
+        return x
+
+
+class Disc(nn.Module):
+    def __init__(
+        self,
+        n_dim=3,
+        l_dim=10,
+        hidden=300,
+        num_layers=3,
+        num_heads=1,
+        n_part=2,
+        fc=False,
+        dropout=0.5,
+        mass=False,
+        clf=False,
+    ):
+        super().__init__()
+        self.hidden_nodes = hidden
+        self.n_dim = n_dim
+        #         l_dim=n_dim
+        self.l_dim = l_dim
+        self.n_part = n_part
+        self.fc = fc
+        self.clf = clf
+
+        if fc:
+            self.l_dim *= n_part
+            self.embbed_flat = nn.Linear(n_dim * n_part, l_dim)
+            self.flat_hidden = nn.Linear(l_dim, hidden)
+            self.flat_hidden2 = nn.Linear(hidden, hidden)
+            self.flat_hidden3 = nn.Linear(hidden, hidden)
+            self.flat_out = nn.Linear(hidden, 1)
+        else:
+            self.embbed = nn.Linear(n_dim, l_dim)
+            self.encoder = nn.TransformerEncoder(
+                nn.TransformerEncoderLayer(
+                    d_model=self.l_dim,
+                    nhead=num_heads,
+                    dim_feedforward=hidden,
+                    dropout=dropout,
+                    norm_first=False,
+                    activation=lambda x: leaky_relu(x, 0.2),
+                    batch_first=True,
+                ),
+                num_layers=num_layers,
+            )
+            self.hidden = nn.Linear(l_dim + int(mass), 2 * hidden)
+            self.hidden2 = nn.Linear(2 * hidden, hidden)
+            self.out = nn.Linear(hidden, 1)
+
+    def forward(self, x, m=None, mask=None):
+        if self.fc:
+            x = x.reshape(len(x), self.n_dim * self.n_part)
+            x = self.embbed_flat(x)
+            x = leaky_relu(self.flat_hidden(x), 0.2)
+            x = leaky_relu(self.flat_hidden2(x), 0.2)
+            x = self.flat_out(x)
+        else:
+            x = self.embbed(x)
+            if self.clf:
+                x = torch.concat(
+                    (torch.ones_like(x[:, 0, :]).reshape(len(x), 1, -1), x), axis=1
+                )
+                mask = torch.concat(
+                    (torch.ones_like((mask[:, 0]).reshape(len(x), 1)), mask), dim=1
+                ).to(x.device)
+
+                x = self.encoder(x, src_key_padding_mask=mask)
+                x = x[:, 0, :]
+            else:
+                x = self.encoder(x, src_key_padding_mask=mask)
+                x = torch.sum(x, axis=1)
+            if m is not None:
+                x = torch.concat((m.reshape(len(x), 1), x), axis=1)
+            x = leaky_relu(self.hidden(x), 0.2)
+            x = leaky_relu(self.hidden2(x), 0.2)
+            x = self.out(x)
+            x = x
+        return x
--- fgsim/models/disc/disc_gat.py
+++ fgsim/models/disc/disc_gat.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GATv2Conv, GINConv, global_add_pool, knn_graph
+import torch_geometric.nn as pygnn
 from torch_geometric.utils import to_dense_batch
 
 from fgsim.models.common.ffn import FFN
@@ -18,8 +18,9 @@ class ModelClass(nn.Module):
         self.n_cond = n_cond
 
         self.rgan_down_dim = 4
-        self.gnn_dim = 5
+        self.gnn_dim = 30
         self.n_heads = 3
+        self.k = 30
 
         features = [self.n_features, 16, 32, 64]
         self.fc_layers = nn.ModuleList(
@@ -34,25 +35,30 @@ class ModelClass(nn.Module):
         self.lin = FFN(
             self.n_features + self.rgan_down_dim + self.n_cond,
             self.gnn_dim,
-            n_layers=2,
+            # n_layers=2,
         )
-        self.gin1 = GINConv(FFN(self.gnn_dim, self.gnn_dim, n_layers=2))
-        self.gat = GATv2Conv(self.gnn_dim, self.gnn_dim, heads=self.n_heads)
-        self.gin2 = GINConv(
-            FFN(self.gnn_dim * self.n_heads, self.gnn_dim, n_layers=2)
-        )
+        self.gin1 = pygnn.GINConv(FFN(self.gnn_dim, self.gnn_dim))
+        self.mpdown1 = FFN(self.gnn_dim + self.gnn_dim, self.gnn_dim)
+        # self.gat = pygnn.GATv2Conv(self.gnn_dim, self.gnn_dim, heads=self.n_heads)
+        self.gin2 = pygnn.GINConv(FFN(self.gnn_dim, self.gnn_dim))
+        self.mpdown2 = FFN(self.gnn_dim + self.gnn_dim, self.gnn_dim)
 
-        self.final_layer = FFN(self.rgan_down_dim + self.gnn_dim + self.n_cond, 1)
+        self.final_layer = FFN(
+            self.rgan_down_dim + self.gnn_dim + self.n_cond, 1, final_linear=False
+        )
 
     def forward(self, batch, cond):
         n_features = batch.x.shape[1]
         batch_size = batch.batch[-1] + 1
 
-        edge_index = knn_graph(batch.x, 8, batch.batch, loop=True)
         cond = cond.reshape(batch_size, self.n_cond)
         # rgan part
         feat = (
-            to_dense_batch(batch.x)[0]  # pad the feature vector
+            to_dense_batch(
+                batch.x, max_num_nodes=self.n_points, batch_size=batch_size
+            )[
+                0
+            ]  # pad the feature vector
             .reshape(batch_size, self.n_points, n_features)
             .transpose(1, 2)
         )
@@ -63,15 +69,21 @@ class ModelClass(nn.Module):
         feat = F.max_pool1d(input=feat, kernel_size=max_hits).squeeze(-1)
         rganout = self.rgan_downproj(feat)
 
+        # gnn part
         x = self.lin(
             torch.hstack((batch.x, cond[batch.batch], rganout[batch.batch]))
         )
-        edge_index = knn_graph(x, 8, batch.batch, loop=True)
-        x = self.gin1(x, edge_index)
-        x = self.gat(x, edge_index)
-        x = self.gin2(x, edge_index)
-        gnnout = global_add_pool(x, batch=batch.batch, size=batch_size)
+        edge_index = pygnn.knn_graph(x, 8, batch.batch, loop=True)
+        mpout = self.gin1(x, edge_index)
+        mpaggr = pygnn.global_add_pool(mpout, batch=batch.batch, size=batch_size)
+        x = self.mpdown1(torch.hstack((x, mpaggr[batch.batch])))
 
-        out = self.final_layer(torch.hstack((rganout, cond, gnnout))).squeeze()
+        # x = self.gat(x, edge_index)
+        mpout = self.gin2(x, edge_index)
+        mpaggr = pygnn.global_add_pool(mpout, batch=batch.batch, size=batch_size)
+        x = self.mpdown2(torch.hstack((x, mpaggr[batch.batch])))
+        ggnout = pygnn.global_add_pool(x, batch=batch.batch, size=batch_size)
+
+        out = self.final_layer(torch.hstack((rganout, cond, ggnout))).squeeze()
         assert not torch.any(torch.isnan(out))
         return out
