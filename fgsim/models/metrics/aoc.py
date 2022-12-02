802a9b2e444f35b4d8ae5121c57d959150216639
mova <mova@users.noreply.github.com>
2022-12-02 15:28:26 +0100

    fix aoc, add w1disc

--- fgsim/models/metrics/aoc.py
+++ fgsim/models/metrics/aoc.py
@@ -1,33 +1,24 @@
 import torch
 from sklearn.metrics import roc_auc_score
 
-from fgsim.config import conf, device
-
 
 class Metric:
     def __init__(
         self,
     ):
-        real_label = torch.ones(
-            (conf.loader.batch_size,), dtype=torch.float, device=device
-        )
-        fake_label = torch.zeros(
-            (conf.loader.batch_size,), dtype=torch.float, device=device
-        )
-        self.y_true = torch.hstack([real_label, fake_label]).detach().cpu().numpy()
+        pass
 
     def __call__(self, d_gen: torch.Tensor, d_sim: torch.Tensor, **kwargs) -> float:
-        sim_mean_disc = (
-            d_sim.reshape(-1, conf.loader.batch_size).mean(dim=0).detach().cpu()
-        )
-        gen_mean_disc = (
-            d_gen.reshape(-1, conf.loader.batch_size).mean(dim=0).detach().cpu()
-        )
-        y_pred = (
-            torch.sigmoid(torch.hstack([sim_mean_disc, gen_mean_disc])).numpy()
-            > 0.5
-        )
-        return float(roc_auc_score(self.y_true, y_pred))
+        assert d_sim.shape == d_gen.shape
+        assert d_sim.dim() == 2 and d_sim.shape[1] == 1
+        sim_mean_disc = d_sim.detach().cpu().reshape(-1)[:2000]
+        gen_mean_disc = d_gen.detach().cpu().reshape(-1)[:2000]
+
+        y_pred = torch.sigmoid(torch.hstack([sim_mean_disc, gen_mean_disc])).numpy()
+        y_true = torch.hstack(
+            [torch.ones_like(sim_mean_disc), torch.zeros_like(gen_mean_disc)]
+        ).numpy()
+        return float(roc_auc_score(y_true, y_pred))
 
 
 aoc = Metric()
---
+++ fgsim/models/metrics/w1disc.py
@@ -0,0 +1,17 @@
+import torch
+from scipy.stats import wasserstein_distance
+
+
+class Metric:
+    def __init__(
+        self,
+    ):
+        pass
+
+    def __call__(self, d_gen: torch.Tensor, d_sim: torch.Tensor, **kwargs) -> float:
+        assert d_sim.shape == d_gen.shape
+        assert d_sim.dim() == 2 and d_sim.shape[1] == 1
+        return wasserstein_distance(
+            d_gen.detach().cpu().numpy().reshape(-1)[:2000],
+            d_sim.detach().cpu().numpy().reshape(-1)[:2000],
+        )
