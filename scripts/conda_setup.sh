1f199c5c22ee8cb38b0039608077b2b47cc6d2b4
mova <mova@users.noreply.github.com>
2023-03-08 16:40:46 +0100

    reorganize conda, install miniconda

--- scripts/conda_setup.sh
+++ scripts/conda_setup.sh
@@ -4,18 +4,26 @@ set -ex
 export CONDA_DIR=~/beegfs/conda/miniconda
 export RAMDIR=/dev/shm/${USER}/fgsim
 export TARBALL_PATH=~/fgsim/env.tar
-export TARBALL_BASE_PATH=~/beegfs/conda/fgsim_base.tar
 mkdir -p /dev/shm/${USER}
+
+if [[ ! -f  ${CONDA_DIR}/bin/activate ]]; then
+    mkdir -p ${CONDA_DIR}
+    curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | sh -b -s -p ${CONDA_DIR}
+fi
+
+
+source ${CONDA_DIR}/bin/activate
 
 conda config --set auto_activate_base false
 conda config --append channels conda-forge
+# Add the user directory in ram to the list of env locations
+conda config --append envs_dirs /dev/shm/${USER}/
 
 # remove modules
 if ! command -v module &> /dev/null
 then
     module purge
 fi
-
 
 if ! command -v mamba &> /dev/null
 then
@@ -26,70 +34,47 @@ then
     conda install mamba --yes
 fi
 
-
 # Init conda and mamba by hand
 set +x
 eval "$(${CONDA_DIR}/bin/conda shell.bash hook 2> /dev/null)"
 source ${CONDA_DIR}/etc/profile.d/mamba.sh
 set -x
-# Add the user directory in ram to the list of env locations
-conda config --append envs_dirs /dev/shm/${USER}/
+
 # Create the env
 mamba create --yes --prefix /dev/shm/${USER}/fgsim python=3.9
-
-# Init conda and mamba by hand
-set +x
-eval "$(${CONDA_DIR}/bin/conda shell.bash hook 2> /dev/null)"
-source ${CONDA_DIR}/etc/profile.d/mamba.sh
-set -x
 
 mamba activate ${RAMDIR}
 conda config --add channels pytorch
 conda config --add channels pyg
-conda config --add channels comet_ml
 
-
-
-mamba install --yes  -c pytorch  cudatoolkit=11.6
-mamba install --yes numpy
+mamba install --yes  pytorch pytorch-cuda=11.6 -c pytorch -c nvidia
 python -c 'import torch; assert torch.cuda.is_available()'
-# mamba install glib
-mamba install --yes pyg -c pyg
-# Assert that torch scatter works
-python -c 'import torch_scatter'
 
-
-#Project dependencies
-# mamba install --yes omegaconf typeguard tqdm uproot awkward  tblib  scikit-learn multiprocessing-logging  icecream prettytable pretty_errors
-# mamba install --yes tensorboard  pytorch-lightning
-mamba install --yes omegaconf typeguard tqdm uproot awkward tensorboard tblib pytorch-lightning scikit-learn multiprocessing-logging  icecream prettytable pretty_errors torchinfo seaborn sqlitedict
-mamba install --yes ray-tune hyperopt
-mamba install --yes -c comet_ml comet_ml
-# dev tools
-mamba install --yes black isort flake8 mypy pytest pre-commit ipykernel jupyter notebook openai
-
-# jetnet requirements
-mamba install --yes coffea h5py wurlitzer
+mamba install --yes omegaconf typeguard tqdm uproot awkward tensorboard tblib pytorch-lightning scikit-learn multiprocessing-logging   prettytable pretty_errors torchinfo seaborn sqlitedict wandbray-tune hyperopt
 
 # crypto vs openssl bug
 # https://stackoverflow.com/questions/74981558/error-updating-python3-pip-attributeerror-module-lib-has-no-attribute-openss
 mamba install cryptography==38.0.4
 
+# dev tools
+mamba install --yes black isort flake8 mypy pytest pre-commit ipykernel jupyter notebook openai icecream
+
+## PyG
+mamba install --yes pyg -c pyg
+# Assert that torch scatter works
+python -c 'import torch_scatter'
+# remove torch-spline-conv that causes an error
+# python -c  'from torch_spline_conv import spline_basis, spline_weighting'
+pip uninstall torch-spline-conv
+
+# jetnet requirements
+mamba install --yes coffea h5py wurlitzer
+
 pip install jetnet
-pip install wandb
-
-
-# mamba pack -n fgsim -o ${TARBALL_BASE_PATH}
-
-tar -c -f ${TARBALL_BASE_PATH} --directory=${RAMDIR} .
 
 # activate the enviroment in ram
 unalias -a
 source ${RAMDIR}/bin/activate
-
-# remove torch-spline-conv that causes an error
-# python -c  'from torch_spline_conv import spline_basis, spline_weighting'
-pip uninstall torch-spline-conv
 
 pip install -e ~/fgsim
 
