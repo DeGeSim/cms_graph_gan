57338eaca2cccec34aba7747a0c844aa68fc90d7
mova <mova@users.noreply.github.com>
2023-03-23 14:34:23 +0100

    discdeeptree new baseline with BipartPool and linear embedding

--- fgsim/models/disc/disc_deeptree.py
+++ fgsim/models/disc/disc_deeptree.py
@@ -1,9 +1,12 @@
 # noqa: F401
 import torch
-from torch import nn
-from torch_geometric.nn import global_add_pool, knn_graph
+import torch.nn.functional as F
+from torch import Tensor, nn
+from torch_geometric.nn import conv, global_add_pool, knn_graph
 from torch_geometric.nn.pool import SAGPooling, TopKPooling, global_max_pool
+from torch_geometric.utils import to_dense_adj, to_dense_batch
 
+from fgsim.config import conf
 from fgsim.models.common import FFN, MPLSeq
 
 ffn_param = {
@@ -39,44 +42,103 @@ class ModelClass(nn.Module):
                 )
             )
             self.pools.append(
-                TopKPooling(
+                BipartPool(
                     in_channels=self.features[ilevel + 1],
                     ratio=self.nodes[ilevel + 1],
                 )
-                # SAGPooling(
-                #     self.features[ilevel + 1],
-                #     ratio=self.nodes[ilevel + 1],
-                # )
             )
-            self.pcdiscs.append(TSumTDisc(self.features[ilevel + 1]))
-        self.last_level_disc = TSumTDisc(self.features[-1])
+        for ilevel in range(self.n_levels):
+            self.pcdiscs.append(TSumTDisc(self.features[ilevel]))
 
     def forward(self, batch, condition):
         x: torch.Tensor
         x, batchidx = batch.x, batch.batch
         x_disc = torch.zeros((batch.num_graphs, 1), dtype=x.dtype, device=x.device)
 
-        # aggregate latent space features
         x_lat_list = []
-        x_lat_list.append(global_add_pool(x, batchidx))
-        x_lat_list.append(global_max_pool(x, batchidx))
 
-        for ilevel in range(self.n_levels - 1):
-            x = self.embeddings[ilevel](x, batchidx, condition)
-
+        for ilevel in range(self.n_levels):
+            # aggregate latent space features
             x_lat_list.append(global_add_pool(x, batchidx))
             x_lat_list.append(global_max_pool(x, batchidx))
 
             x_disc += self.pcdiscs[ilevel](x, batchidx)
-            _, _, _, batchidx, perm, _ = self.pools[ilevel](
-                x=x,
+
+            if ilevel == self.n_levels - 1:
+                break
+            x = self.embeddings[ilevel](x, batchidx, condition)
+
+            x, _, _, batchidx, _, _ = self.pools[ilevel](
+                x=x.clone(),
                 edge_index=torch.empty(2, 0, dtype=torch.long, device=x.device),
                 batch=batchidx,
             )
-            x = x[perm]
+            assert x.shape == (
+                conf.loader.batch_size * self.pools[ilevel].ratio,
+                self.features[ilevel + 1],
+            )
 
-        x_disc += self.last_level_disc(x, batchidx)
         return x_disc, torch.hstack(x_lat_list)
+
+
+class BipartPool(nn.Module):
+    def __init__(self, in_channels, ratio) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.ratio = ratio
+        self.heads = 4
+
+        self.xcent_base = nn.Parameter(
+            torch.normal(0, 1, size=(self.ratio, self.in_channels))
+        )
+        # self.cent_emb_nn = torch.nn.Sequential(
+        #     nn.Linear(in_channels, in_channels, bias=False),
+        #     nn.BatchNorm1d(in_channels),
+        #     nn.LeakyReLU(0.1),
+        #     nn.Linear(in_channels, in_channels, bias=False),
+        #     nn.BatchNorm1d(in_channels),
+        # )
+        self.mpl = conv.GATv2Conv(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            heads=self.heads,
+            concat=False,
+        )
+
+    def forward(self, x: Tensor, edge_index: Tensor, batch: Tensor):
+        batch_size = batch[-1] + 1
+        target_graph_size = batch_size * self.ratio
+        source_graph_size = len(x)
+
+        source = torch.arange(
+            source_graph_size, device=x.device, dtype=torch.long
+        ).repeat_interleave(self.ratio)
+
+        target = torch.arange(self.ratio, device=x.device, dtype=torch.long).repeat(
+            source_graph_size
+        )
+        # shift for the batchidx
+        target += batch.repeat_interleave(self.ratio) * self.ratio
+        # assert len(source) == len(target) == source_graph_size * self.ratio
+        # assert ((0 <= source) & (source < source_graph_size)).all()
+        # assert ((0 <= target) & (target < target_graph_size)).all()
+        # assert max(source) + 1 == source_graph_size
+        # assert max(target) + 1 == target_graph_size
+        # tcounts = (
+        #     target.unique(return_counts=True)[1].reshape(batch_size, self.ratio).T
+        # )
+        # assert (tcounts[0] == tcounts).all()
+
+        ei_o2c = torch.vstack([source, target])
+
+        xcent = self.mpl(
+            x=(x.clone(), self.xcent_base.repeat(batch_size, 1)),
+            edge_index=ei_o2c,
+        )
+        self.batchcent = torch.arange(
+            batch_size, device=x.device, dtype=torch.long
+        ).repeat_interleave(self.ratio)
+        return xcent, None, None, self.batchcent, None, None
 
 
 class TSumTDisc(nn.Module):
@@ -137,22 +199,22 @@ class Embedding(nn.Module):
         self.n_ftx_out = n_ftx_out
 
         # self.space_emb = FFN(n_ftx_in, n_ftx_latent, **ffn_param)
-        self.mpls = MPLSeq(
-            "GINConv",
-            self.n_ftx_in,
-            self.n_ftx_latent,
-            skip_connecton=True,
-            n_hidden_nodes=self.n_ftx_latent,
-            layer_param={
-                k: v for k, v in ffn_param.items() if k != "hidden_layer_size"
-            }
-            | {"norm": "batchnorm"},
-            n_global=0,
-            n_cond=5,
-            n_mpl=2,
-        )
+        # self.mpls = MPLSeq(
+        #     "GINConv",
+        #     self.n_ftx_in,
+        #     self.n_ftx_latent,
+        #     skip_connecton=True,
+        #     n_hidden_nodes=self.n_ftx_latent,
+        #     layer_param={
+        #         k: v for k, v in ffn_param.items() if k != "hidden_layer_size"
+        #     }
+        #     | {"norm": "batchnorm"},
+        #     n_global=0,
+        #     n_cond=5,
+        #     n_mpl=2,
+        # )
         self.out_emb = FFN(
-            self.n_ftx_latent,
+            self.n_ftx_in,
             self.n_ftx_out,
             **(ffn_param | {"bias": True, "norm": "batchnorm"}),
             final_linear=True,
@@ -160,8 +222,8 @@ class Embedding(nn.Module):
 
     def forward(self, x, batch, condition):
         # x = self.space_emb(x)
-        ei = knn_graph(x[..., : self.n_ftx_space], batch=batch, k=5)
-        x = self.mpls(x=x, edge_index=ei, batch=batch, cond=condition)
+        # ei = knn_graph(x[..., : self.n_ftx_space], batch=batch, k=5)
+        # x = self.mpls(x=x, edge_index=ei, batch=batch, cond=condition)
         x_emb = self.out_emb(x)
         return x_emb
 
