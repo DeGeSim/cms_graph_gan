3055b4b1d2193127ceb63ff573265e06707b018b
mova <mova@users.noreply.github.com>
2023-04-07 15:56:15 +0200

    use ray tune restore

--- raytune/tune.py
+++ raytune/tune.py
@@ -25,46 +25,53 @@ def trial_dirname_creator(trial):
     return str(trial)
 
 
-# local = False
-local = True
+local = False
+# local = True
 print(f"start tune in local model: {local}")
 
 if local:
     ray.init(local_mode=True)
 else:
     ray.init("auto")
-tuner = tune.Tuner(
-    tune.with_resources(MyTrainable, {"gpu": 1}),
-    param_space=hyperpars,
-    run_config=air.RunConfig(
-        callbacks=[WandbLoggerCallback(project=run_name)],
-        log_to_file=True,
-        name=run_name,
-        local_dir="~/fgsim/wd/ray/",
-        stop=ExperimentPlateauStopper(metric="fpnd"),
-        failure_config=air.FailureConfig(fail_fast="raise" if local else False),
-        checkpoint_config=air.CheckpointConfig(
-            num_to_keep=2,
-            checkpoint_score_attribute="fpnd",
-            checkpoint_frequency=5,
-            checkpoint_at_end=True,
-            checkpoint_score_order="min",
+try:
+    tuner = ray.tune.Tuner.restore(
+        path=f"~/fgsim/wd/ray/{run_name}",
+        trainable=tune.with_resources(MyTrainable, {"gpu": 1}),
+        param_space=hyperpars,
+    )
+except Exception:
+    tuner = tune.Tuner(
+        tune.with_resources(MyTrainable, {"gpu": 1}),
+        param_space=hyperpars,
+        run_config=air.RunConfig(
+            callbacks=[WandbLoggerCallback(project=run_name)],
+            log_to_file=True,
+            name=run_name,
+            local_dir="~/fgsim/wd/ray/",
+            stop=ExperimentPlateauStopper(metric="fpnd"),
+            failure_config=air.FailureConfig(fail_fast="raise" if local else False),
+            checkpoint_config=air.CheckpointConfig(
+                num_to_keep=2,
+                checkpoint_score_attribute="fpnd",
+                checkpoint_frequency=5,
+                checkpoint_at_end=True,
+                checkpoint_score_order="min",
+            ),
+            sync_config=tune.SyncConfig(syncer=None),  # Disable syncing
         ),
-        sync_config=tune.SyncConfig(syncer=None),  # Disable syncing
-    ),
-    tune_config=tune.TuneConfig(
-        mode="min",
-        metric="fpnd",
-        scheduler=MedianStoppingRule(
-            time_attr="training_iteration", grace_period=110.0
+        tune_config=tune.TuneConfig(
+            mode="min",
+            metric="fpnd",
+            scheduler=MedianStoppingRule(
+                time_attr="training_iteration", grace_period=110.0
+            ),
+            search_alg=HyperOptSearch(),
+            num_samples=-1,
+            trial_name_creator=trial_name_id,
+            trial_dirname_creator=trial_dirname_creator,
+            chdir_to_trial_dir=False,
         ),
-        search_alg=HyperOptSearch(),
-        num_samples=-1,
-        trial_name_creator=trial_name_id,
-        trial_dirname_creator=trial_dirname_creator,
-        chdir_to_trial_dir=False,
-    ),
-)
+    )
 # try:
 #     tuner = tuner.restore(f"~/fgsim/wd/ray/{run_name}")
 # except:
