689b4a138a60dd55b476a4500419423c36472dfe
mova <mova@users.noreply.github.com>
2023-05-28 11:16:23 +0200

    disc benno cc -> mdma

--- fgsim/models/disc/disc_mdma.py
+++ fgsim/models/disc/disc_mdma.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.nn.utils.weight_norm as weight_norm
 
 from fgsim.models.common.benno import WeightNormalizedLinear
@@ -16,9 +15,7 @@ class ModelClass(nn.Module):
 
     def forward(self, batch, cond):
         x = to_stacked_mask(batch)
-        x, x_cls = self.net(
-            x[..., :-1], mask=(1 - x[..., -1]).bool(), cond=cond[:, [-1]]
-        )
+        x, x_cls = self.net(x[..., :-1], mask=(1 - x[..., -1]).bool(), cond=cond)
         assert not torch.isnan(x).any()
         assert not torch.isnan(x_cls).any()
         return {
@@ -27,68 +24,45 @@ class ModelClass(nn.Module):
         }
 
 
-class Block(nn.Module):
-    def __init__(self, embed_dim, num_heads, hidden, dropout, weightnorm=True):
+class BlockCls(nn.Module):
+    def __init__(self, embed_dim, num_heads, hidden, dropout):
         super().__init__()
-        self.fc0 = (
-            WeightNormalizedLinear(embed_dim, hidden)
-            if weightnorm
-            else nn.Linear(embed_dim, hidden)
-        )
-        self.fc0_cls = (
-            (WeightNormalizedLinear(embed_dim + n_cond, hidden))
-            if weightnorm
-            else nn.Linear(embed_dim + n_cond, hidden)
-        )
-        self.fc1 = (
-            (WeightNormalizedLinear(hidden + embed_dim, embed_dim))
-            if weightnorm
-            else nn.Linear(hidden + embed_dim, embed_dim)
-        )
-        self.fc1_cls = (
-            (WeightNormalizedLinear(hidden + 1, embed_dim))
-            if weightnorm
-            else nn.Linear(hidden + 1, embed_dim)
-        )
-        self.cond_cls = nn.Linear(1, embed_dim)
+
+        self.fc0 = WeightNormalizedLinear(embed_dim, hidden)
+        self.fc1 = WeightNormalizedLinear(hidden + embed_dim, embed_dim)
+        self.fc1_cls = WeightNormalizedLinear(hidden + 1, embed_dim)
         self.attn = weight_norm(
             nn.MultiheadAttention(
                 hidden, num_heads, batch_first=True, dropout=dropout
             ),
             "in_proj_weight",
         )
+
         self.act = nn.LeakyReLU()
         self.ln = nn.LayerNorm(hidden)
+        self.hidden = hidden
 
-    def forward(self, x, x_cls, cond, mask, weight=False):
-        res = x.clone()
-        res_cls = x_cls.clone()
+    def forward(self, x, x_cls, mask, weight=False):
+        res = x_cls.clone()
         x = self.act(self.fc0(x))
-        x_cls = self.act(self.ln(self.fc0_cls(torch.cat((x_cls, cond), dim=-1))))
+        x_cls = self.act(self.ln(self.fc0(x_cls)))
         if weight:
             x_cls, w = self.attn(x_cls, x, x, key_padding_mask=mask)
         else:
-            x_cls, w = self.attn(
-                x_cls, x, x, key_padding_mask=mask, need_weights=False
-            )
+            x_cls = self.attn(x_cls, x, x, key_padding_mask=mask)[0]
+            w = None
         x_cls = self.act(
             self.fc1_cls(
                 torch.cat(
-                    (x_cls, mask.float().sum(1).unsqueeze(1).unsqueeze(1) / 4000),
-                    dim=-1,
+                    (x_cls, mask.sum(1).unsqueeze(1).unsqueeze(1) / 70), dim=-1
                 )
             )
-        )  # +x.mean(dim=1).
-        x_cls = self.act(
-            F.glu(torch.cat((x_cls, self.cond_cls(cond[:, :, :1])), dim=-1))
         )
         x = self.act(
-            self.fc1(torch.cat((x, x_cls.expand(-1, x.shape[1], -1)), dim=-1)) + res
+            self.fc1(torch.cat((x, x_cls.expand(-1, x.shape[1], -1)), dim=-1))
         )
-        x_cls = x_cls + res_cls
-        # x=F.glu(torch.cat((x,self.cond(cond[:,:,:1])
-        # .expand(-1,x.shape[1],-1)),dim=-1))
-        return x, x_cls, w
+        x_cls = x_cls + res
+        return x_cls, x, w
 
 
 class Disc(nn.Module):
@@ -97,18 +71,14 @@ class Disc(nn.Module):
         self.embbed = WeightNormalizedLinear(n_dim, l_dim)
         self.encoder = nn.ModuleList(
             [
-                Block(
-                    embed_dim=l_dim,
-                    num_heads=heads,
-                    hidden=hidden,
-                    dropout=dropout,
-                    weightnorm=True,
+                BlockCls(
+                    embed_dim=l_dim, num_heads=heads, hidden=hidden, dropout=dropout
                 )
                 for i in range(num_layers)
             ]
         )
         self.out = WeightNormalizedLinear(l_dim, 1)
-        self.embbed_cls = WeightNormalizedLinear(l_dim + n_cond, l_dim)
+        self.embbed_cls = WeightNormalizedLinear(l_dim + 1, l_dim)
         self.cls_token = nn.Parameter(torch.randn(1, 1, l_dim), requires_grad=True)
         self.act = nn.LeakyReLU()
         self.fc1 = WeightNormalizedLinear(l_dim, hidden)
@@ -117,16 +87,18 @@ class Disc(nn.Module):
 
     def forward(self, x, mask, cond, weight=False):  # mean_field=False
         ws = []
-        cond = cond.unsqueeze(1)
         x = self.act(self.embbed(x))
         x_cls = torch.cat(
-            ((x.sum(1) / 4000).unsqueeze(1).clone(), cond), dim=-1
+            (
+                x.mean(1).unsqueeze(1).clone(),
+                mask.sum(1).unsqueeze(1).unsqueeze(1) / 70,
+            ),
+            dim=-1,
         )  # self.cls_token.expand(x.size(0), 1, -1)
         x_cls = self.act(self.embbed_cls(x_cls))
         for layer in self.encoder:
-            x, x_cls, w = layer(x, x_cls=x_cls, mask=mask, cond=cond, weight=weight)
+            x_cls, x, w = layer(x, x_cls=x_cls, mask=mask, weight=weight)
             res = x_cls.clone()
-
             x_cls = self.act(x_cls)
             if weight:
                 ws.append(w)
