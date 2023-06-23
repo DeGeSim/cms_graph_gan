fix gradient plotting

fgsim/models/gen/gen_deeptree.py
@@ -79,7 +79,7 @@ class ModelClass(nn.Module):
             ]
         )
 
-        self.prebr_hlvs = nn.ModuleList(
+        self.hlvs_nn = nn.ModuleList(
             [
                 DynHLVsLayer(
                     n_features=nfeatures,
@@ -198,7 +198,7 @@ class ModelClass(nn.Module):
                 ]
             ).all()
             ftx_level = graph_tree.tftx_by_level(ilevel)
-            graph_tree.global_features = self.prebr_hlvs[ilevel](
+            graph_tree.global_features = self.hlvs_nn[ilevel](
                 x=ftx_level, cond=cond, batch=batch_level
             )
 

fgsim/monitoring/metrics_aggr.py
@@ -25,11 +25,13 @@ class GradHistAggregator:
     def __init__(self) -> None:
         self.metric_collector = OrderedDict()
         self.history = OrderedDict()
+        self.steps = deque()
         # self.max_memory = 10
 
-    def aggregate(self):
+    def aggregate(self, step):
         aggr_dict = {k: np.mean(v) for k, v in self.metric_collector.items()}
         self.append_dict_(self.history, aggr_dict)
+        self.steps.append(step)
         # if len(list(self.history.values())[0]) > self.max_memory:
         #     self.compress_history()
         self.metric_collector = OrderedDict()

fgsim/plot/modelgrads.py
@@ -1,8 +1,10 @@
 import math
+from collections import OrderedDict
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
+from matplotlib.figure import Figure
 
 
 def get_grad_dict(model):
@@ -13,19 +15,26 @@ def get_grad_dict(model):
     for n, p in named_parameters:
         if (p.requires_grad) and ("bias" not in n):
             layers.append(
-                n.rstrip("_orig")
-                .rstrip(".weight")
-                .rstrip(".linear")
+                n.replace(".parametrizations.weight.orig", ".snorm")
+                .rstrip("_orig")
+                .rstrip("_weight")
                 .replace(".seq.", ".")
                 .replace(".nn.", ".")
+                .replace("_nn.", ".")
+                .replace(".reduction_nn.", ".red.")
+                .replace(".proj_nn.", ".proj.")
+                .rstrip(".linear")
             )
             ave_grads.append(p.grad.abs().mean().cpu())
             # max_grads.append(p.grad.abs().max().cpu())
     return {k: v for k, v in zip(layers, ave_grads)}
 
 
-def fig_grads(graddict, partname) -> plt.Figure:
+def fig_grads(grad_aggr, partname: str) -> Figure:
+    graddict: OrderedDict = grad_aggr.history
+
     # plt.title(f"{var}, all gen values are nan")
+    steps = np.array(grad_aggr.steps)
     layers = list(graddict.keys())
     ave_grads = np.array([list(e) for e in graddict.values()])
 
@@ -46,35 +55,36 @@ def fig_grads(graddict, partname) -> plt.Figure:
     ]
     layers_formated = ["/".join(e) for e in layers_split]
 
-    max_bins = 3
-    nparts, timesteps_pre = ave_grads.shape
-    if timesteps_pre > max_bins:
-        pad_to = math.ceil(timesteps_pre / max_bins) * max_bins
-        ave_grads = np.pad(
-            ave_grads,
-            pad_width=((0, 0), (0, pad_to - timesteps_pre)),
-            constant_values=np.nan,
-        )
-        ave_grads = ave_grads.reshape(nparts, -1, max_bins)[..., 0]
-    timesteps = ave_grads.shape[-1]
+    max_bins = 50
 
-    xaxis = np.arange(nparts)
+    ave_grads = pad_to_multiple(ave_grads, max_bins)
+    steps = pad_to_multiple(steps.reshape(1, -1), max_bins).reshape(-1)
+    nparts, ntimesteps = ave_grads.shape
 
     plt.clf()
-    fig = plt.figure(figsize=(20 + timesteps / 2, 24))
+    fig = plt.figure(figsize=(24, 24))
 
-    plt.imshow(
-        ave_grads,
-        cmap=plt.cm.coolwarm,
-        norm=LogNorm(),
-    )
-    plt.yticks(ticks=xaxis, labels=layers_formated, family="monospace")
-    plt.xticks(np.arange(timesteps))
+    plt.imshow(ave_grads, cmap=plt.cm.coolwarm, norm=LogNorm())
+    plt.yticks(ticks=np.arange(nparts), labels=layers_formated, family="monospace")
+    plt.xticks(ticks=np.arange(ntimesteps), labels=steps, rotation=45)
     plt.ylabel("Layers")
-    plt.xlabel("Time")
+    plt.xlabel("Step")
     plt.colorbar()
     plt.title(f"{partname} Gradient Scale")
     plt.tight_layout()
     # plt.savefig("wd/modelgrads.pdf")
 
     return fig
+
+
+def pad_to_multiple(arr: np.ndarray, max_bins: int):
+    nparts, nentries = arr.shape
+    if nentries <= max_bins:
+        return arr
+    pad_to = math.ceil(nentries / max_bins) * max_bins
+    arr = np.pad(
+        arr,
+        pad_width=((0, 0), (0, pad_to - nentries)),
+        constant_values=np.nan,
+    )
+    return arr.reshape(nparts, -1, max_bins)[..., 0]