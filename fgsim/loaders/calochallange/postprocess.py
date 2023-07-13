e57834d7d45a8acaa0a23bcc1b64be27a802a150
mova <mova@users.noreply.github.com>
2023-07-13 11:20:30 +0200

    refactor analyze layer code

--- fgsim/loaders/calochallange/postprocess.py
+++ fgsim/loaders/calochallange/postprocess.py
@@ -4,7 +4,7 @@ from fgsim.config import conf
 
 from .convcoord import batch_to_Exyz
 from .pca import fpc_from_batch
-from .shower import analyze_shower, cone_ratio, response
+from .shower import analyze_layers, cone_ratio, response
 
 
 def postprocess(batch: Batch) -> Batch:
@@ -15,7 +15,7 @@ def postprocess(batch: Batch) -> Batch:
     if "fpc" in metrics:
         batch["fpc"] = fpc_from_batch(batch)
     if "showershape" in metrics:
-        batch["showershape"] = analyze_shower(batch)
+        batch["showershape"] = analyze_layers(batch)
     if "response" in metrics:
         batch["response"] = response(batch)
 
--- fgsim/loaders/calochallange/shower.py
+++ fgsim/loaders/calochallange/shower.py
@@ -5,80 +5,71 @@ from torch_geometric.nn.pool import global_add_pool, global_max_pool
 from fgsim.config import conf
 
 
-def analyze_shower(batch: Batch) -> dict[str, torch.Tensor]:
+def analyze_layers(batch: Batch) -> dict[str, torch.Tensor]:
     """Get the ratio between half-turnon and
     half turnoff to peak center from a PC"""
     batchidx = batch.batch
+    device = batch.x.device
     z = batch.xyz[..., -1]
-    global_max_pool(z, batchidx)
+
+    # global_max_pool(z, batchidx)
 
     zvals = z.unique()
-    if len(zvals) > 100:
+    n_layers = len(zvals)
+    if n_layers > 100:
         raise RuntimeError("To many layers")
 
     # Construct the matrices to compare which zvalue belongs to which layer
     zval_mat = zvals.reshape(1, -1).repeat(len(z), 1)
-    z_mat = z.reshape(-1, 1).repeat(1, len(zvals))
+    z_mat = z.reshape(-1, 1).repeat(1, n_layers)
 
     # compute the histogramm with dim (events,layers)
     hist = global_add_pool((zval_mat == z_mat).float(), batchidx)
     del zval_mat, z_mat
     assert (hist.sum(1) == batch.ptr[1:] - batch.ptr[:-1]).all()
 
-    # # Produce the zshape
-    # zshape = hist.sum(0)
+    max_hits_per_event, peak_layer_per_event = hist.max(1)
 
-    # # Produce the zshape centered by the peak
-    peak_hits, peak_layer = hist.max(1)
     # produce an index for each layer in each event
-    centered_layers_perevent = (
-        torch.arange(len(zvals))
-        .to(z.device)
-        .reshape(1, -1)
-        .repeat(len(peak_layer), 1)
-    )
-    # shift peak to number of layers
-    centered_layers_perevent += -peak_layer.reshape(-1, 1) + len(zvals)
+    # centered_layers_perevent = (
+    #     torch.arange(n_layers).to(device).reshape(1, -1).repeat(n_events, 1)
+    # )
+    # # shift peak to number of layers
+    # centered_layers_perevent += -peak_layer_per_event.reshape(-1, 1) + n_layers
+    # # separate idx by event
+    # eventshift = (
+    #     (torch.arange(n_events) * n_layers * 2)
+    #     .to(device)
+    #     .reshape(-1, 1)
+    #     .repeat(1, n_layers)
+    # )
+    # centered_layers_perevent += eventshift
 
     # # aggegate for each of the shifted layers
     # zshape_centered = global_add_pool(
     #     hist.reshape(-1), centered_layers_perevent.reshape(-1)
     # )
 
-    # Plot to check
-    # import matplotlib.pyplot as plt
-
-    # fig, ax = plt.subplots()
-    # ax.plot(zshape_centered.cpu(), marker="o", linestyle="none")
-    # ax.axvline(len(zvals))
-    # ax.axhline(zshape.max().cpu() / 2)
-    # fig.savefig("wd/zshape_centered.png")
-
-    # fig, ax = plt.subplots()
-    # ax.plot(zshape.cpu())
-    # fig.savefig("wd/zshape.png")
-
     # # Estimate the turnon/turnoff width
     # get a boolean matrix to find the layers with enough hits
-    occupied_layers = hist > peak_hits.reshape(-1, 1).repeat(1, len(zvals)) / 2
+    occupied_layers = (
+        hist > max_hits_per_event.reshape(-1, 1).repeat(1, n_layers) / 2
+    )
     eventidx, occ_layer = occupied_layers.nonzero().T
     # find the min and max layer from the bool matrix ath the same time
     minmax = global_max_pool(torch.stack([occ_layer, -occ_layer]).T, eventidx)
     # reverse the -1 trick
-    turnoff_layer, turnon_layer = (minmax * torch.tensor([1, -1]).to(z.device)).T
+    turnoff_layer, turnon_layer = (minmax * torch.tensor([1, -1]).to(device)).T
+
     # distance to the peak
-    turnoff_dist = turnoff_layer - peak_layer
-    turnon_dist = peak_layer - turnon_layer
+    turnoff_dist = turnoff_layer - peak_layer_per_event
+    turnon_dist = peak_layer_per_event - turnon_layer
     psr = (turnoff_dist + 1) / (turnon_dist + 1)
-    # print((turnoff_dist > turnon_dist).float().mean())
 
     return {
-        "peak_layer": peak_layer.float(),
+        "peak_layer": peak_layer_per_event.float(),
         "psr": psr.float(),
         "turnon_layer": turnon_layer.float(),
-        # "shape": zshape, # wrong shape
-        # "zshape_centered": zshape_centered, wrong shape
-        # TODO
     }
 
 
