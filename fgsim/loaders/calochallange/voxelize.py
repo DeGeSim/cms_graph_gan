500834719ad148c31ffc4403a298f27dd3e54e38
mova <mova@users.noreply.github.com>
2023-08-04 12:20:19 +0200

    implement scattering hits

--- fgsim/loaders/calochallange/voxelize.py
+++ fgsim/loaders/calochallange/voxelize.py
@@ -1,18 +1,17 @@
 from math import prod
 
 import torch
+from torch_geometric.data import Batch
 from torch_scatter import scatter_add
 
 from fgsim.config import conf, device
 from fgsim.io.batch_tools import fix_slice_dict_nodeattr
 from fgsim.utils.batch import ptr_from_batchidx
 
-from .objcol import scaler
-
-num_z = 45
-num_alpha = 16
-num_r = 9
+num_z, num_alpha, num_r = 45, 16, 9
+# num_z, num_alpha, num_r = 2, 2, 1
 dims = [num_z, num_alpha, num_r]
+
 
 cell_idxs = torch.arange(prod(dims)).reshape(*dims)
 full_event_cell_idx = (
@@ -49,6 +48,8 @@ def voxelize(batch):
 
 
 def invscale_add_position(batch):
+    from .objcol import scaler
+
     if batch.pos is not None:
         if len(batch.x) == len(batch.pos):
             return
@@ -87,41 +88,60 @@ def cell_occ_per_hit(batch):
     return batch_occ
 
 
-def sum_dublicate_hits(batch, forbid_dublicates=False):
-    old_dev = batch.x.device
-    batch = batch.to("cpu")
-    dev = batch.x.device
-    batchidx = batch.batch
-    n_events = int(batchidx[-1] + 1)
-
-    hitE = batch.x[:, 0]
-    pos = batch.x[:, 1:].long()
-
+def __globalidx_from_pos(pos, batchidx):
     pos_z, pos_alpha, pos_r = pos.T
     assert (pos >= 0).all()
     assert (pos_z < num_z).all()
     assert (pos_alpha < num_alpha).all()
     assert (pos_r < num_r).all()
-
-    cell_idx_per_hit = cell_idxs.to(dev)[pos_z, pos_alpha, pos_r]
-
-    # give pos unique values for each event
+    dev = pos.device
+    n_events = int(batchidx[-1] + 1)
+    # create and index that indexes each cell for each event
+    globalidx = cell_idxs.to(dev)[pos_z, pos_alpha, pos_r]
+    # add the shiftvector to diffentiate for each event
     eventshift = torch.arange(n_events).to(dev) * prod(dims)
-    event_and_cell_idxs = cell_idx_per_hit + eventshift[batchidx]
+    globalidx = globalidx + eventshift[batchidx]
+    return globalidx, eventshift
 
-    # sort the event_and_cell_idxs
-    cell_idx_per_hit, index_perm = _scatter_sort(event_and_cell_idxs, batchidx)
+
+def sum_dublicate_hits(batch, forbid_dublicates=False, shiftmultihit=True):
+    old_dev = batch.x.device
+    # batch = batch.to("cpu")
+
+    batchidx = batch.batch
+    assert (batchidx.diff() >= 0).all()
+    globalidx, eventshift = __globalidx_from_pos(batch.x[:, 1:].long(), batchidx)
+
+    if shiftmultihit:
+        # get new positions and global index
+        # and the current index of these events
+        batch, globalidx = _move_doublehits_to_neighbor_cells(
+            batch,
+            globalidx.clone(),
+            eventshift.clone(),
+        )
+
+    # sort the globalidx
+    globalidx, index_perm = _scatter_sort(globalidx, batchidx)
+
+    hitE = batch.x[:, 0]
+    pos = batch.x[:, 1:].long()
     hitE = hitE[index_perm]
     pos = pos[index_perm]
     assert (batchidx[index_perm] == batchidx).all()
 
-    _, invidx, counts = torch.unique_consecutive(
-        cell_idx_per_hit, return_inverse=True, return_counts=True
+    # unique_cells_idx counts up every time a new cell in an
+    # even is accessed in globalidx
+    # counts gives the times the cell/event idx is occupied
+    _, unique_cells_idx, counts = torch.unique(
+        globalidx, return_inverse=True, return_counts=True
     )
+
     if forbid_dublicates:
         assert (counts - 1 == 0).all()
 
-    hitE_new = scatter_add(hitE, invidx)
+    # begin sum
+    hitE_new = scatter_add(hitE, unique_cells_idx)
     sel_new_idx = counts.cumsum(-1) - 1
     if forbid_dublicates:
         assert (sel_new_idx == torch.arange(len(batch.x))).all()
@@ -137,18 +157,20 @@ def sum_dublicate_hits(batch, forbid_dublicates=False):
 
     x_new = torch.hstack([hitE_new.reshape(-1, 1), pos_new])
 
-    # # Tests
-    # old_counts = torch.unique_consecutive(batchidx, return_counts=True)[1]
-    # if "n_pointsv" in batch.keys:
-    #     assert (old_counts == batch.n_pointsv).all()
-    # assert ((old_counts - new_counts) == n_multihit).all()
-    # assert torch.allclose(
-    #     scatter_add(hitE_new, batchidx_new), scatter_add(hitE, batchidx)
-    # )
-    # assert torch.allclose(
-    #     scatter_add(pos_new * hitE_new.unsqueeze(-1), batchidx_new, -2),
-    #     scatter_add(pos * hitE.unsqueeze(-1), batchidx, -2),
-    # )
+    # TODO remove sanity test:
+    old_counts = torch.unique_consecutive(batchidx, return_counts=True)[1]
+    if "n_pointsv" in batch.keys:
+        assert (old_counts == batch.n_pointsv).all()
+    assert ((old_counts - new_counts) == n_multihit).all()
+    assert torch.allclose(
+        scatter_add(hitE_new, batchidx_new), scatter_add(hitE, batchidx)
+    )
+    if not shiftmultihit:
+        assert torch.allclose(
+            scatter_add(pos_new * hitE_new.unsqueeze(-1), batchidx_new, -2),
+            scatter_add(pos * hitE.unsqueeze(-1), batchidx, -2),
+        )
+
     if forbid_dublicates:
         assert (n_multihit == 0).all()
         assert (batch.batch == batchidx_new).all()
@@ -172,12 +194,113 @@ def sum_dublicate_hits(batch, forbid_dublicates=False):
     return batch.to(old_dev)
 
 
+def _move_doublehits_to_neighbor_cells(
+    batch: Batch, globalidx: torch.Tensor, eventshift: torch.Tensor
+):
+    pos: torch.Tensor = batch.x[:, 1:].long()
+    batchidx: torch.Tensor = batch.batch
+
+    dev = pos.device
+
+    _, unique_cells_idx, counts = torch.unique(
+        globalidx, return_inverse=True, return_counts=True
+    )
+    # select the second hit in each cell for moving around
+    # TODO change to lowest eneergy hit
+    mhit_idxs = (
+        torch.cat([torch.tensor([0]), 1 - unique_cells_idx.diff()])
+        .nonzero()
+        .squeeze()
+    )
+    shift_options = len(dims) * 2
+    # start value for each shift
+    shift_state = torch.randint_like(mhit_idxs, 0, shift_options)
+    idxs_to_overwrite = []
+    new_pos_list = []
+    new_global_list = []
+
+    for _ in range(shift_options):
+        if len(mhit_idxs) == 0:
+            break
+        new_pos = _shift_pos(pos[mhit_idxs], shift_state)
+        # get the new double index for the shifted hits
+        new_global = (
+            cell_idxs.to(dev)[
+                new_pos.T[0],
+                new_pos.T[1],
+                new_pos.T[2],
+            ]
+            + eventshift[batchidx[mhit_idxs]]
+        )
+        # The new position is valid iff it's A) not occupied
+        new_pos_is_free = torch.isin(
+            new_global, torch.cat([globalidx, *new_global_list])
+        )
+
+        # and B) this is the first hit to be shifted to this cell
+        # we now need and index that filters mhit_idxs
+        # and only lets the first though.
+        # (4,3,4,1,3)   -> (1,1,0,1,0) or (0,1,3)
+        # we can check if the index  of new_global.unique is increaseing
+        # but for this we need to sort the indexes again
+        new_global, perm = new_global.sort()
+        mhit_idxs = mhit_idxs[perm]
+        new_pos = new_pos[perm]
+
+        # now we can check of the unique rev index is increasing
+        # which tells us of the cell is being accessed for the first time
+        first_new_hit = torch.cat(
+            (
+                torch.tensor([1]),
+                new_global.unique(return_inverse=True)[1].diff(),
+            )
+        ).bool()
+
+        # combine the two
+        valide_shift_index = new_pos_is_free & first_new_hit
+
+        # apppend the shifted hits to the list
+        if valide_shift_index.sum():
+            idxs_to_overwrite.append(mhit_idxs[valide_shift_index])
+            new_pos_list.append(new_pos[valide_shift_index])
+            new_global_list.append(new_global[valide_shift_index])
+        # rerun loop with the remaining multihits
+        mhit_idxs = mhit_idxs[~valide_shift_index]
+        shift_state = (shift_state[~valide_shift_index] + 1) % shift_options
+
+    if len(idxs_to_overwrite):
+        # overwrite old position and global index
+        stacked_idxs_to_overwrite = torch.cat(idxs_to_overwrite)
+        globalidx[stacked_idxs_to_overwrite] = torch.cat(new_global_list)
+        pos[stacked_idxs_to_overwrite] = torch.cat(new_pos_list)
+
+        batch.x[:, 1:] = pos
+    return batch, globalidx
+
+
 def _scatter_sort(x, index, dim=-1):
     x, x_perm = torch.sort(x, dim=dim)
     index = index.take_along_dim(x_perm, dim=dim)
     index, index_perm = torch.sort(index, dim=dim, stable=True)
     x = x.take_along_dim(index_perm, dim=dim)
     return x, x_perm.take_along_dim(index_perm, dim=dim)
+
+
+def _shift_pos(pos, shift_state):
+    pos = pos.clone()
+    directon = (shift_state % 2) * 2 - 1
+    dim = shift_state // 2
+    pos[torch.arange(len(pos)), dim] += directon
+
+    # rotate alpha
+    alphas = pos[:, 1]
+    alphas[alphas > num_alpha - 1] -= num_alpha
+    alphas[alphas < 0] -= num_alpha
+    pos[:, 1] = alphas
+
+    # clamp r and z
+    pos = torch.clamp(pos, torch.tensor([0, 0, 0]), torch.tensor(dims) - 1)
+    return pos
 
 
 def test_sum_dublicate_hits():
@@ -189,24 +312,28 @@ def test_sum_dublicate_hits():
                 x=torch.tensor(
                     [
                         [1, 0, 0, 0],
-                        [1, 0, 1, 0],
-                        [1, 0, 0, 0],
-                        [1, num_z - 1, num_alpha - 1, num_r - 1],
                     ]
                 ),
                 y=torch.tensor([[1, 1]]),
-                n_pointsv=torch.tensor(4),
+                # n_pointsv=torch.tensor(3),
             ),
             Data(
                 x=torch.tensor(
                     [
-                        [1, 0, 1, 0],
                         [1, 0, 0, 0],
-                        [1, 0, 1, 0],
+                        [1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 1, 1, 0],
+                        [1, 1, 1, 0],
+                        # [1, 1, 0, 0],
+                        # [1, 1, 0, 0],
+                        # [1, 0, 0, 0],
+                        # [1, num_z - 1, num_alpha - 1, num_r - 1],
                     ]
                 ),
                 y=torch.tensor([[1, 1]]),
-                n_pointsv=torch.tensor(3),
+                # n_pointsv=torch.tensor(4),
             ),
         ]
     )
@@ -236,4 +363,5 @@ def test_sum_dublicate_hits():
     )
 
 
-test_sum_dublicate_hits()
+if __name__ == "__main__":
+    test_sum_dublicate_hits()
