6828cf83c1e6aa2c38afd2d8b38a399df8339900
mova <mova@users.noreply.github.com>
2023-09-29 11:47:16 +0200

    jn metric std weighted average

--- fgsim/models/metrics/jetnet.py
+++ fgsim/models/metrics/jetnet.py
@@ -1,63 +1,46 @@
-import jetnet
 import numpy as np
 import torch
+from jetnet.evaluation import gen_metrics
 from torch_geometric.data import Batch
 
 from fgsim.config import conf
 from fgsim.datasets.jetnet.utils import to_stacked_mask
 
 num_batches = 1 if conf.command == "train" else 5
-num_eval_samples = 50_000
+num_eval_samples = 25_000
 jnkw = {"num_batches": num_batches, "num_eval_samples": num_eval_samples}
 
 
 def w1m(gen_batch: Batch, sim_batch: Batch, **kwargs) -> tuple[float, float]:
-    jets1 = to_stacked_mask(gen_batch).detach().cpu().numpy()
-    jets2 = to_stacked_mask(sim_batch).detach().cpu().numpy()
-    jf1 = jets1[..., :3]
-    jf2 = jets2[..., :3]
-    score = jetnet.evaluation.gen_metrics.w1m(jets1=jf1, jets2=jf2, **jnkw)
-    return tuple(min(float(e) * 1e3, 1e5) for e in score)
+    jf1 = get_jf_mask(gen_batch)[0]
+    jf2 = get_jf_mask(sim_batch)[0]
+    score = gen_metrics.w1m(jets1=jf1, jets2=jf2, **jnkw)
+    return bound_res(score)
 
 
 def w1p(gen_batch: Batch, sim_batch: Batch, **kwargs) -> tuple[float, float]:
-    jets1 = to_stacked_mask(gen_batch).detach().cpu().numpy()
-    jets2 = to_stacked_mask(sim_batch).detach().cpu().numpy()
-    jf1, jf2 = (jets1[..., :3], jets2[..., :3])
-    mask1, mask2 = (jets1[..., -1], jets2[..., -1])
-
-    score = jetnet.evaluation.gen_metrics.w1p(
+    jf1, mask1 = get_jf_mask(gen_batch)
+    jf2, mask2 = get_jf_mask(sim_batch)
+    res = gen_metrics.w1p(
         jets1=jf1, jets2=jf2, mask1=mask1, mask2=mask2, exclude_zeros=True, **jnkw
     )
-    #  array of length num_particle_features
-    # containing average W1 scores for each feature.
-    # ["etarel", "phirel", "ptrel", "mask"]
-    return (
-        min(float(score[0].mean()) * 1e3, 1e5),
-        min(float(np.sqrt((score[1] ** 2).sum()) * 1e3), 1e5),
-    )
+    score, error = std_weighted_mean(res)
+    return bound_res([score, error])
 
 
 def w1efp(gen_batch: Batch, sim_batch: Batch, **kwargs) -> tuple[float, float]:
-    jets1 = to_stacked_mask(gen_batch).detach().cpu().numpy()
-    jets2 = to_stacked_mask(sim_batch).detach().cpu().numpy()
-    jf1, jf2 = (jets1[..., :3], jets2[..., :3])
-    score = jetnet.evaluation.gen_metrics.w1efp(
-        jets1=jf1, jets2=jf2, efp_jobs=10, **jnkw
-    )
-    return tuple(min(float(np.mean(e)) * 1e5, 1e5) for e in score)
+    jf1, _ = get_jf_mask(gen_batch)
+    jf2, _ = get_jf_mask(sim_batch)
+    res = gen_metrics.w1efp(jets1=jf1, jets2=jf2, efp_jobs=10, **jnkw)
+    score, error = std_weighted_mean(res)
+    return bound_res([score, error], 1e5)
 
 
 def fpnd(gen_batch: Batch, **kwargs) -> float:
     jets = to_stacked_mask(gen_batch)[:, :, :3]
     if conf.loader.n_points != 30:
         pts = jets[..., conf.loader.x_ftx_energy_pos]
-        topidxs = pts.topk(
-            k=30,
-            dim=1,
-            largest=True,
-            # sorted=True,
-        ).indices
+        topidxs = pts.topk(k=30, dim=1, largest=True).indices
 
         highptjets = torch.stack([jet[idx] for jet, idx in zip(jets, topidxs)])
 
@@ -65,7 +48,7 @@ def fpnd(gen_batch: Batch, **kwargs) -> float:
         highptjets = jets
 
     try:
-        score = jetnet.evaluation.gen_metrics.fpnd(
+        score = gen_metrics.fpnd(
             jets=highptjets,
             jet_type=conf.loader.jettype,
             use_tqdm=False,
@@ -81,33 +64,52 @@ def cov_mmd(gen_batch: Batch, sim_batch: Batch, **kwargs):
         gen_jets = to_stacked_mask(gen_batch)
         if gen_jets.isnan().any():
             raise ValueError
-        score_cov, score_mmd = jetnet.evaluation.gen_metrics.cov_mmd(
+        score_cov, score_mmd = gen_metrics.cov_mmd(
             real_jets=real_jets,
             gen_jets=gen_jets,
             use_tqdm=False,
             num_batches=num_batches,
         )
-        return min(float(score_cov), 1e5), min(float(score_mmd), 1e5)
+        return bound_res([score_cov, score_mmd], 1e5)
     except ValueError:
         return (1e5, 1e5)
 
 
 def kpd(gen_batch: Batch, sim_batch: Batch, **kwargs) -> tuple[float, float]:
-    sim_efps, gen_efps = sim_batch.efps, gen_batch.efps
-    score = jetnet.evaluation.gen_metrics.kpd(
-        real_features=sim_efps,
-        gen_features=gen_efps,
+    res = gen_metrics.kpd(
+        real_features=sim_batch.efps[:num_eval_samples],
+        gen_features=gen_batch.efps[:num_eval_samples],
         num_threads=10,
         num_batches=num_batches,
     )
-    return tuple(min(float(e) * 1e3, 1e5) for e in score)
+    return bound_res(res, 1e3)
 
 
 def fpd(gen_batch: Batch, sim_batch: Batch, **kwargs) -> tuple[float, float]:
-    sim_efps, gen_efps = sim_batch.efps, gen_batch.efps
-    score = jetnet.evaluation.gen_metrics.fpd(
-        real_features=sim_efps,
-        gen_features=gen_efps,
+    res = gen_metrics.fpd(
+        real_features=sim_batch.efps[:num_eval_samples],
+        gen_features=gen_batch.efps[:num_eval_samples],
         num_batches=num_batches,
     )
-    return tuple(min(float(e) * 1e3, 1e5) for e in score)
+    return bound_res(res, 1e3)
+
+
+def get_jf_mask(batch):
+    jets = to_stacked_mask(batch).detach().cpu().numpy()
+    jf = jets[:num_eval_samples, ..., :3]
+    mask = jets[:num_eval_samples, ..., -1]
+    return jf, mask
+
+
+def bound_res(res, scale=1e3):
+    assert len(res) == 2
+    return tuple(min(float(e), 1) * scale for e in res)
+
+
+def std_weighted_mean(res):
+    # https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
+    x, stds = res[0], res[1]
+    ws = stds**-2
+    score = (x * ws).sum() / ws.sum()
+    error = np.sqrt((1 / ws.sum()))
+    return score, error
